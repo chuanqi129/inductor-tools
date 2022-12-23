0621bcb10f6aad91950758a0c0fad309195ecfa1
chuanqiw <chuanqi.wang@intel.com>
2022-12-23 05:28:26 -0800

    Add a workaround for microbenchmark

--- scripts/microbench/microbench.sh
+++ scripts/microbench/microbench.sh
@@ -8,6 +8,9 @@ export OMP_NUM_THREADS=$CORES
 LOG_DIR=${1:-dynamo_ww48_5}
 mkdir -p $LOG_DIR
 
+# workaround for microbenchmark
+sed -i "s;raise e;#raise e;"  benchmarks/dynamo/microbenchmarks/operatorbench.py
+
 timestamp=`date +%Y%m%d_%H%M%S`
 numactl -C 0-$end_core --membind 0 python benchmarks/dynamo/microbenchmarks/operatorbench.py --suite torchbench --op all --dtype float32 --repeats 30 --device cpu 2>&1 | tee ${LOG_DIR}/multi_threads_opbench_torchbench_${timestamp}.log
 numactl -C 0-$end_core --membind 0 python benchmarks/dynamo/microbenchmarks/operatorbench.py --suite huggingface --op all --dtype float32 --repeats 30 --device cpu  2>&1 | tee ${LOG_DIR}/multi_threads_opbench_huggingface_${timestamp}.log
