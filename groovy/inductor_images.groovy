90f991f10f88928fd4340522df1c271cf1a4b059
ESI-SYD <yudong.si@intel.com>
2022-12-28 14:55:01 +0800

    update

--- groovy/inductor_images.groovy
+++ groovy/inductor_images.groovy
@@ -14,7 +14,7 @@ node(NODE_LABEL){
             #!/usr/bin/env bash
             cd ${WORKSPACE}
             rm -rf tmp
-            git clone https://github.com/chuanqi129/inductor-tools.git tmp
+            git clone -b ${inductor_tools_branch} https://github.com/chuanqi129/inductor-tools.git tmp
             mv tmp/docker/Dockerfile ./
             mv tmp/scripts/microbench/microbench_parser.py ./
             mv tmp/scripts/microbench/microbench.sh ./
@@ -32,8 +32,8 @@ node(NODE_LABEL){
         if [ -n "${old_image}" ]; then
             docker tag ${old_image} ccr-registry.caas.intel.com/pytorch/pt_inductor:nightly_previously
             docker rmi -f ccr-registry.caas.intel.com/pytorch/pt_inductor:nightly
-        fi        
-        DOCKER_BUILDKIT=1 docker build --no-cache --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg PT_REPO=${PT_REPO} --build-arg PT_BRANCH=${PT_BRANCH} --build-arg TORCH_VISION_COMMIT=${TORCH_VISION_COMMIT} --build-arg TORCH_DATA_COMMIT=${TORCH_DATA_COMMIT} --build-arg TORCH_TEXT_COMMIT=${TORCH_TEXT_COMMIT} --build-arg TORCH_AUDIO_COMMIT=${TORCH_AUDIO_COMMIT} --build-arg TORCH_BENCH_COMMIT=${TORCH_BENCH_COMMIT} -t ccr-registry.caas.intel.com/pytorch/pt_inductor:nightly -f Dockerfile --target image .
+        fi
+        DOCKER_BUILDKIT=1 docker build --no-cache --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg PT_REPO=${PT_REPO} --build-arg PT_BRANCH=${PT_BRANCH} --build-arg PT_COMMIT=${PT_COMMIT} --build-arg TORCH_VISION_BRANCH=${TORCH_VISION_BRANCH} --build-arg TORCH_VISION_COMMIT=${TORCH_VISION_COMMIT} --build-arg TORCH_DATA_BRANCH=${TORCH_DATA_BRANCH} --build-arg TORCH_DATA_COMMIT=${TORCH_DATA_COMMIT} --build-arg TORCH_TEXT_BRANCH=${TORCH_TEXT_BRANCH} --build-arg TORCH_TEXT_COMMIT=${TORCH_TEXT_COMMIT} --build-arg TORCH_AUDIO_BRANCH=${TORCH_AUDIO_BRANCH} --build-arg TORCH_AUDIO_COMMIT=${TORCH_AUDIO_COMMIT} --build-arg TORCH_BENCH_BRANCH=${TORCH_BENCH_BRANCH} --build-arg TORCH_BENCH_COMMIT=${TORCH_BENCH_COMMIT} --build-arg BENCH_COMMIT=${BENCH_COMMIT} -t ccr-registry.caas.intel.com/pytorch/pt_inductor:nightly -f Dockerfile --target image .
         '''
     }
 
