4fc530c0a65abbee1cf65d73c393e0d31fce7685
ESI-SYD <yudong.si@intel.com>
2023-02-23 17:45:14 +0800

    align with lasted config

--- scripts/microbench/microbench.sh
+++ scripts/microbench/microbench.sh
@@ -1,6 +1,5 @@
 export LD_PRELOAD=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libiomp5.so:${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libjemalloc.so
-export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
-export USE_LLVM=False
+export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:-1,muzzy_decay_ms:-1"
 
 CORES=$(lscpu | grep Core | awk '{print $4}')
 end_core=$(expr $CORES - 1)
