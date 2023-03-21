5565d4205bf33a41a511f5556412e1efb2a0f44b
Wang, Chuanqi <chuanqi.wang@intel.com>
2023-03-21 12:53:13 +0800

    Add dynamic shape parameter in scripts (#8)

--- scripts/cosim/inductor_cosim.sh
+++ scripts/cosim/inductor_cosim.sh
@@ -5,8 +5,9 @@ SUITE=${1:-huggingface}
 MODEL=${2:-GoogleFnet}
 CHANNELS=${3:-first}
 DT=${4:-float32}
-BS=${5:-0}
-LOG_DIR=${6:-debug}
+SHAPE=${5:-static}
+BS=${6:-0}
+LOG_DIR=${7:-debug}
 export TORCH_COMPILE_DEBUG=1
 
 if [[ $USER == "" ]]; then
@@ -17,5 +18,5 @@ inductor_codegen_path="/tmp/torchinductor_$USER"
 log_path=${LOG_DIR}/${SUITE}_${MODEL}
 rm -rf ${log_path}
 mkdir -p $log_path
-bash ./inductor_single_run.sh ${SUITE} ${MODEL} ${CHANNELS} ${DT} ${BS} 2>&1 | tee ${log_path}/raw.log
+bash ./inductor_single_run.sh ${SUITE} ${MODEL} ${CHANNELS} ${DT} ${SHAPE} ${BS} 2>&1 | tee ${log_path}/raw.log
 python ./inductor_cosim.py --dbg_log ${log_path}/raw.log --dbg_code_src_path $inductor_codegen_path --dbg_code_dst_path $log_path/debug --dbg_code_dst_single_readable_py ${log_path}/graph.py
