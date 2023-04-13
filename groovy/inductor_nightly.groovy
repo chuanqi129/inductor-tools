d19f9f460239bf7154f1e059bbdedc210cf59eb8
ESI-SYD <yudong.si@intel.com>
2023-04-13 11:00:43 +0800

    update cache

--- groovy/inductor_nightly.groovy
+++ groovy/inductor_nightly.groovy
@@ -389,10 +389,10 @@ node(NODE_LABEL){
             sh '''
             #!/usr/bin/env bash
             tag=${image_tag}
-            docker run -tid --name $USER --privileged --env https_proxy=${https_proxy} --env http_proxy=${http_proxy} --net host  --shm-size 1G -v /home/torch/.cache/torch/hub/checkpoints/:/root/.cache/torch/hub/checkpoints -v ${WORKSPACE}/inductor_log:/workspace/pytorch/inductor_log ${DOCKER_IMAGE_NAMESPACE}:${tag}
+            docker run -tid --name $USER --privileged --env https_proxy=${https_proxy} --env http_proxy=${http_proxy} --net host  --shm-size 1G -v /home/torch/.cache:/root/.cache -v ${WORKSPACE}/inductor_log:/workspace/pytorch/inductor_log ${DOCKER_IMAGE_NAMESPACE}:${tag}
             docker cp scripts/modelbench/inductor_test.sh $USER:/workspace/pytorch
             docker cp scripts/modelbench/log_parser.py $USER:/workspace/pytorch
-            docker exec -i $USER bash -c "bash inductor_test.sh ${THREAD} ${CHANNELS} ${DT} ${SHAPE} inductor_log ${MODEL_SUITE};python log_parser.py --target inductor_log -m ${THREAD};cp inductor_dashboard_regression_check.xlsx inductor_log"
+            docker exec -i $USER bash -c "bash inductor_test.sh ${THREAD} ${CHANNELS} ${DT} ${SHAPE} inductor_log ${MODEL_SUITE};pip install styleframe;python log_parser.py --target inductor_log -m ${THREAD};cp inductor_dashboard_regression_check.xlsx inductor_log"
             '''
         }
     }
