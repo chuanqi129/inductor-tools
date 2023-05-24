d9c772298d83e8e826dcc3f49f0b4ac1a1fd0f2b
ESI-SYD <yudong.si@intel.com>
2023-05-24 14:23:31 +0800

    Update inductor_aws_benchmark.groovy

--- groovy/inductor_aws_benchmark.groovy
+++ groovy/inductor_aws_benchmark.groovy
@@ -186,7 +186,7 @@ node(NODE_LABEL){
         {
             if(refer_build != '0') {
                 copyArtifacts(
-                    projectaws_id: currentBuild.projectName,
+                    projectName: currentBuild.projectName,
                     selector: specific("${refer_build}"),
                     fingerprintArtifacts: true
                 )           
@@ -206,7 +206,7 @@ node(NODE_LABEL){
         {
             if(refer_build != '0') {
                 copyArtifacts(
-                    projectaws_id: currentBuild.projectName,
+                    projectName: currentBuild.projectName,
                     selector: specific("${refer_build}"),
                     fingerprintArtifacts: true
                 )           
