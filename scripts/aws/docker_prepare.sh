e4c61112581d49219ed35fdcaa0885cba54c08c1
Weizhuo Zhang <weizhuo.zhang@intel.com>
2023-11-02 15:06:12 +0800

    update docker prepare

--- scripts/aws/docker_prepare.sh
+++ scripts/aws/docker_prepare.sh
@@ -6,6 +6,7 @@ sudo snap install docker
 if [[ ! $(groups "${USER}" | grep -q docker) ]]; then
   sudo groupadd docker
   sudo gpasswd -a ${USER} docker
+  sudo chmod 666 /var/run/docker.sock
   sudo su
   exit
   newgrp docker <<TEST
