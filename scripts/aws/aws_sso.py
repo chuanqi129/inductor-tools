745d16ce0231cebbb5afd0ef79a7919ead171368
chuanqiw <chuanqi.wang@intel.com>
2023-11-09 10:36:30 -0800

    fix aws sso refresh issue

--- scripts/aws/aws_sso.py
+++ scripts/aws/aws_sso.py
@@ -5,9 +5,11 @@
 # You need to specify the FirefoxBinary and executable_path explicitly to advoid use default outdated one
 
 import argparse
+import sys
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -26,14 +28,42 @@ url = "https://device.sso.us-west-2.amazonaws.com/?user_code="
 def authorize_request(browser):
     # aws login approval
     print("APPROV...")
+    sleep(10)
     approval= '//*[@id="cli_login_button"]/span'
-    WebDriverWait(browser, 200).until(EC.element_to_be_clickable((By.XPATH, approval)))   
-    sleep(10)       
-    browser.find_element(By.XPATH, approval).click()
+    get_element_refresh(browser, approval, "click", 5)
     # success='//*[@id="LoginForm"]/div/span'
     # WebDriverWait(browser, 60).until(EC.visibility_of_element_located((By.XPATH, success)))
     print("AWS SSO Refresh Done")
     browser.quit()
+
+def get_element_refresh(driver, ele_reg, op, max_try, param=""):
+    try_num = 0
+    sleep_time = 10
+    while(True):
+        sleep(sleep_time)
+        try:
+            elem = driver.find_element(By.XPATH, ele_reg)
+        except NoSuchElementException as e:
+            print(e)
+            driver.refresh()
+            print("refreshing...")
+            sleep_time += 10
+            try_num += 1
+            if (try_num == max_try):
+                print("Timeout...")
+                sys.exit(2)
+        else:
+            print("Get the element!")
+            if (op == "send_keys"):
+                WebDriverWait(driver, 200).until(EC.visibility_of_element_located((By.XPATH, ele_reg)))
+                elem.send_keys(param)
+            elif (op == "click"):
+                WebDriverWait(driver, 200).until(EC.element_to_be_clickable((By.XPATH, ele_reg)))
+                elem.click()
+            else:
+                print("op not support!!")
+            #getattr(elem, op)(param)
+            break
 
 def sso_refresh(ff,gd,code,user,passwd):
     options = Options()
@@ -49,30 +79,30 @@ def sso_refresh(ff,gd,code,user,passwd):
         # cli verification for enter code [new]
         print("cli_verification_btn...")
         cvb= '//*[@id="cli_verification_btn"]'
-        WebDriverWait(driver, 200).until(EC.element_to_be_clickable((By.XPATH, cvb)))   
-        sleep(5)       
-        driver.find_element(By.XPATH, cvb).click()        
+        get_element_refresh(driver, cvb, "click", 5)
         # intel azure portal login
         print("IAP...")
         iap='//*[@id="i0116"]'
-        WebDriverWait(driver, 1200).until(EC.visibility_of_element_located((By.XPATH, iap)))
-        driver.find_element(By.XPATH, iap).send_keys(user)
-        
+        get_element_refresh(driver, iap, "send_keys", 10, user)
+
         print("Next...")
-        next='//*[@id="idSIButton9"]'
-        driver.find_element(By.XPATH, next).click()
+        nxt='//*[@id="idSIButton9"]'
+        get_element_refresh(driver, nxt, "click", 5)
+        #driver.find_element(By.XPATH, next).click()
 
         print("PSWD...")
         sleep(5)
         pswd='//*[@id="i0118"]'
-        WebDriverWait(driver, 1200).until(EC.visibility_of_element_located((By.XPATH, pswd)))
-        driver.find_element(By.XPATH, pswd).send_keys(passwd)
+        get_element_refresh(driver, pswd, "send_keys", 10, passwd)
+
+        print("SIGNIN...")
         sleep(5)
         signin='//*[@id="idSIButton9"]'
-        WebDriverWait(driver, 200).until(EC.element_to_be_clickable((By.XPATH, signin)))      
-        driver.find_element(By.XPATH, signin).click()
+        get_element_refresh(driver, signin, "click", 5)
+
         authorize_request(driver)
     except:
+        print("May has exception occur!!")
         authorize_request(driver)
 
 sso_refresh(args.ff,args.gd,args.code,args.user,args.passwd)
