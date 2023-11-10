709b1a55395935e1787140a1b7d2bc93d5c575fd
Wang, Chuanqi <chuanqi.wang@intel.com>
2023-11-10 09:24:05 +0800

    fix aws sso refresh issue (#20)

--- scripts/aws/refresh.sh
+++ scripts/aws/refresh.sh
@@ -2,19 +2,19 @@
 set -x
 FF=${1:-0}
 GD=${2:-0}
-USER=${3:-0}
+IAP=${3:-0}
 PASSWD=${4:-0}
 
 cat /dev/null >nohup.out
 nohup echo -e 'pytorch\n\n\n\n\n\n\n\n' | $aws configure sso --profile pytorch &>nohup.out &
-sleep 8s
-FF_PID=$(ps -ef | grep firefox | grep -v grep | awk '{print $2}' | awk -F '/' '{print $1}')
+sleep 10s
+FF_PID=$(ps -ef | grep firefox/firefox | grep $USER | grep -v refresh | grep -v grep | awk '{print $2}' | awk -F '/' '{print $1}')
 # SSO_PID=$(ps -ef | grep sso | grep -v grep | awk '{print $2}' | awk -F '/' '{print $1}')
-if [ -n "${FF_PID:1}" ]; then
-    kill -9 ${FF_PID:1}
-    echo kill ${FF_PID:1} successful
+if [ -n "${FF_PID}" ]; then
+    kill -9 ${FF_PID}
+    echo kill ${FF_PID} successful
 else
-    echo not running ${FF_PID:1}
+    echo not running ${FF_PID}
 fi
 # if [ -n "${SSO_PID:1}" ]; then
 #     kill -9 ${SSO_PID:1}
@@ -26,7 +26,7 @@ fi
 enter_code=$(grep -A 2 'enter the code' nohup.out | awk 'NR==3')
 echo $enter_code
 if [ -n "${enter_code}" ]; then
-    python aws_sso.py -f ${FF} -d ${GD} -c $enter_code -u ${USER} -p ${PASSWD}
+    python aws_sso.py -f ${FF} -d ${GD} -c $enter_code -u ${IAP} -p ${PASSWD}
 else
     echo Not Found enter_code, SSO configure may be still valid
 fi
