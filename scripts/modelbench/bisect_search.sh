9ab463458e8d9b93cf4c3c2adbeae2ccb03590c6
chuanqiw <chuanqi.wang@intel.com>
2023-11-20 10:09:10 -0800

    Fix bisect test status

--- scripts/modelbench/bisect_search.sh
+++ scripts/modelbench/bisect_search.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set +x
 START_COMMIT=${1:-main}
 END_COMMIT=${2:-main}
 SUITE=${3:-torchbench}
@@ -25,10 +25,12 @@ echo "===============Start searching guilty commit for ${SUITE} ${MODEL} ${MODE}
 cd /workspace/pytorch
 expected_perf=0
 # For perfroamcen drop issue, get the expected performance based on end/good commit
-if [ $SCENARIO == "performacne" ] && [ $KIND == "drop" ]; then
-    git reset --hard HEAD && git checkout main && git checkout ${START_COMMIT} && git submodule sync && git submodule update --init --recursive
-    python setup.py clean && python setup.py develop
-    detected_value=$(bash ./inductor_single_run.sh $THREADS $MODE $SCENARIO $SUITE $MODEL $DT $CHANNELS $SHAPE $WRAPPER $BS $FREEZE | tail -n 1 | awk -F, '{print $5}')
+if [ "$SCENARIO" == "performance" ] && [ "$KIND" == "drop" ]; then
+    # Initial image build with END_COMMIT, no need rebuild
+    # git reset --hard HEAD && git checkout main && git checkout ${END_COMMIT} && git submodule sync && git submodule update --init --recursive
+    # python setup.py clean && python setup.py develop
+    git checkout ${END_COMMIT}
+    detected_value=$(bash ./inductor_single_run.sh $THREADS $MODE $SCENARIO $SUITE $MODEL $PRECISION $CHANNELS $SHAPE $WRAPPER $BS $FREEZE | tail -n 1 | awk -F, '{print $5}')
     expected_perf=$(echo $detected_value | awk '{ printf "%.5f", $1/1000 }')
     echo "Expected performance: $expected_perf s"
 fi
@@ -37,5 +39,6 @@ git reset --hard HEAD && git checkout main && git pull && git checkout ${START_C
 git bisect start ${START_COMMIT} ${END_COMMIT}
 git bisect run ./bisect_run_test.sh $SUITE $MODEL $MODE $PRECISION $SHAPE $WRAPPER $SCENARIO $KIND $THREADS $CHANNELS $FREEZE 0 $expected_perf $BACKEND $EXTRA 2>&1 | tee ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log
 git bisect reset
-guilty_commit=`cat ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log | grep "bisect found first" | awk -F' ' '{ print $9 }'`
-echo ${guitly_commit}
+cat ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log | grep "is the first bad commit" | awk '{ print $1 }' > ${LOG_DIR}/guitly_commit.log
+echo "Start commit: ${START_COMMIT}" >> ${LOG_DIR}/guitly_commit.log
+echo "End commit: ${END_COMMIT}" >> ${LOG_DIR}/guitly_commit.log
