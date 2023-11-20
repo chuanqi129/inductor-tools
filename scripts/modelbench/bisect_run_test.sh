7fa6aba50fc51b909e905e3212124a322b89b2e6
chuanqiw <chuanqi.wang@intel.com>
2023-11-20 02:20:35 -0800

    fix a bug in bisect

--- scripts/modelbench/bisect_run_test.sh
+++ scripts/modelbench/bisect_run_test.sh
@@ -5,7 +5,8 @@ MODEL=${2:-GoogleFnet}
 MODE=${3:-inference} # inference / training
 DT=${4:-float32} # float32 / amp
 SHAPE=${5:-static} # static / dynamic
-SHAPE=${6:-static} # static / dynamic
+WRAPPER=${6:-default} # default / cpp
+SCENARIO=${6:-accuracy} # accuracy / performance
 KIND=${7:-drop} # crash / drop
 THREADS=${8:-multiple} # multiple / single
 CHANNELS=${9:-first} # first / last
@@ -17,16 +18,16 @@ EXTRA=${14}
 
 prepare_test() {
     git reset --hard HEAD && git submodule sync && git submodule update --init --recursive
-    python setup.py clean && python setup.py develop && cd ..
-    rm -rf vision && git clone -b main https://github.com/pytorch/vision.git && cd vision && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/vision.txt` && pip uninstall torchvision -y && python setup.py bdist_wheel && pip install dist/*.whl && cd ..
-    rm -rf data && git clone -b main https://github.com/pytorch/data.git && cd data && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/data.txt`  && pip uninstall torchdata -y && python setup.py bdist_wheel && pip install dist/*.whl && cd ..
-    rm -rf text && git clone -b main https://github.com/pytorch/text.git && cd text && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/text.txt` && pip uninstall torchtext -y && python setup.py bdist_wheel && pip install dist/*.whl && cd ..
-    rm -rf audio && git clone -b main https://github.com/pytorch/audio.git && cd audio && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/audio.txt` && pip uninstall torchaudio -y && python setup.py bdist_wheel && pip install dist/*.whl && cd ..
-    cd benchmark && hit checkout main && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/torchbench.txt`
+    python setup.py clean && python setup.py develop && cd .. && \
+    rm -rf vision && git clone -b main https://github.com/pytorch/vision.git && cd vision && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/vision.txt` && pip uninstall torchvision -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    rm -rf data && git clone -b main https://github.com/pytorch/data.git && cd data && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/data.txt`  && pip uninstall torchdata -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    rm -rf text && git clone -b main https://github.com/pytorch/text.git && cd text && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/text.txt` && pip uninstall torchtext -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    rm -rf audio && git clone -b main https://github.com/pytorch/audio.git && cd audio && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/audio.txt` && pip uninstall torchaudio -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    cd benchmark && git checkout main && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/torchbench.txt` && cd /workspace/pytorch
 }
 
 run_perf_drop_test() {
-    detected_value=$(bash ./inductor_single_run.sh $THREAD $MODE $SCENARIO $SUITE $MODEL $DT $CHANNELS $SHAPE $WRAPPER $BS $FREEZE | tail -n 1 | awk -F, '{print $5}')
+    detected_value=$(bash ./inductor_single_run.sh $THREADS $MODE $SCENARIO $SUITE $MODEL $DT $CHANNELS $SHAPE $WRAPPER $BS $FREEZE | tail -n 1 | awk -F, '{print $5}')
     result=$(echo $detected_value | awk '{ printf "%.5f", $1/1000 }')
     echo "=====result: $result======="
     ratio=$(echo "$result $EXP_PERF" | awk '{ printf "%.2f\n", $1/$2 }')
@@ -41,7 +42,7 @@ run_perf_drop_test() {
 }
 
 run_acc_drop_test() {
-    acc_res=$(bash ./inductor_single_run.sh $THREAD $MODE $SCENARIO $SUITE $MODEL $DT $CHANNELS $SHAPE $WRAPPER $BS $FREEZE | tail -n 1 | awk -F, '{print $4}')
+    acc_res=$(bash ./inductor_single_run.sh $THREADS $MODE $SCENARIO $SUITE $MODEL $DT $CHANNELS $SHAPE $WRAPPER $BS $FREEZE | tail -n 1 | awk -F, '{print $4}')
     echo "=====acc: $acc_res======="
     if [ $acc_res != "pass" ]; then
 	echo "BAD COMMIT!"
@@ -53,7 +54,7 @@ run_acc_drop_test() {
 }
 
 run_crash_test() {
-    bash ./inductor_single_run.sh $THREAD $MODE $SCENARIO $SUITE $MODEL $DT $CHANNELS $SHAPE $WRAPPER $BS $FREEZE
+    bash ./inductor_single_run.sh $THREADS $MODE $SCENARIO $SUITE $MODEL $DT $CHANNELS $SHAPE $WRAPPER $BS $FREEZE
     if [ $? -eq 0 ]; then
 	echo "GOOD COMMIT!"
         exit 0
@@ -71,6 +72,7 @@ else
     echo "PT build failed!"
 fi
 
+cd /workspace/pytorch
 if [ $KIND == "drop" ]; then
     if [ $SCENARIO == "performance" ]; then
         run_perf_drop_test
--- scripts/modelbench/bisect_search.sh
+++ scripts/modelbench/bisect_search.sh
@@ -25,7 +25,7 @@ echo "===============Start searching guilty commit for ${SUITE} ${MODEL} ${MODE}
 cd /workspace/pytorch
 expected_perf=0
 # For perfroamcen drop issue, get the expected performance based on end/good commit
-if [ $SCENARIO == "performacne" && $KIND == "drop" ]; then
+if [ $SCENARIO == "performacne" ] && [ $KIND == "drop" ]; then
     git reset --hard HEAD && git checkout main && git checkout ${START_COMMIT} && git submodule sync && git submodule update --init --recursive
     python setup.py clean && python setup.py develop
     detected_value=$(bash ./inductor_single_run.sh $THREADS $MODE $SCENARIO $SUITE $MODEL $DT $CHANNELS $SHAPE $WRAPPER $BS $FREEZE | tail -n 1 | awk -F, '{print $5}')
@@ -35,7 +35,7 @@ fi
 chmod +x bisect_run_test.sh
 git reset --hard HEAD && git checkout main && git pull && git checkout ${START_COMMIT}
 git bisect start ${START_COMMIT} ${END_COMMIT}
-git bisect run ./bisect_run_test.sh $SUITE $MODEL $TEST_MODE $SCENARIO $PRECISION $TEST_SHAPE $WRAPPER $KIND $THREADS $CHANNELS $FREEZE 0 $expected_perf $BACKEND $EXTRA 2>&1 | tee ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log
+git bisect run ./bisect_run_test.sh $SUITE $MODEL $MODE $PRECISION $SHAPE $WRAPPER $SCENARIO $KIND $THREADS $CHANNELS $FREEZE 0 $expected_perf $BACKEND $EXTRA 2>&1 | tee ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log
 git bisect reset
 guilty_commit=`cat ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log | grep "bisect found first" | awk -F' ' '{ print $9 }'`
 echo ${guitly_commit}
