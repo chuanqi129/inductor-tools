89231b7c12ab1a132e5141809ade791f93914e53
WeizhuoZhang-intel <weizhuo.zhang@intel.com>
2023-12-12 17:17:04 +0800

    remove chmod docker from docker prepare

--- scripts/aws/docker_prepare.sh
+++ scripts/aws/docker_prepare.sh
@@ -6,7 +6,6 @@ sudo apt install docker.io docker-buildx -y && sudo usermod -aG docker ${USER}
 if [[ ! $(groups "${USER}" | grep -q docker) ]]; then
   sudo groupadd docker
   sudo gpasswd -a ${USER} docker
-  sudo chmod 666 /var/run/docker.sock
   sudo su
   exit
   newgrp docker <<TEST
