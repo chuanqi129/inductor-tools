83f3894d78f875f1bf4555ab47025173ac2261c2
chuanqiw <chuanqi.wang@intel.com>
2023-12-26 06:47:24 +0100

    Enable bisect search on non-main branch

--- scripts/modelbench/bisect_search.sh
+++ scripts/modelbench/bisect_search.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 set +x
-START_COMMIT=${1:-main}
-END_COMMIT=${2:-main}
-SUITE=${3:-torchbench}
-MODEL=${4:-resnet50}
-MODE=${5:-inference}
-SCENARIO=${6:-accuracy}
-PRECISION=${7:-float32}
-SHAPE=${8:-static}
-WRAPPER=${9:-default}
-KIND=${10:-crash}
-THREADS=${11:-multiple}
-CHANNELS=${12:-first}
-FREEZE=${13:-on}
-BS=${14:0}
-LOG_DIR=${15:-inductor_log}
-HF_TOKEN=${16:-hf_xxx}
-BACKEND=${17:-inductor}
-PERF_RATIO=${18:-1.1}
-EXTRA=${19}
+TORCH_BRANCH=${1:-main}
+START_COMMIT=${2:-main}
+END_COMMIT=${3:-main}
+SUITE=${4:-torchbench}
+MODEL=${5:-resnet50}
+MODE=${6:-inference}
+SCENARIO=${7:-accuracy}
+PRECISION=${8:-float32}
+SHAPE=${9:-static}
+WRAPPER=${10:-default}
+KIND=${11:-crash}
+THREADS=${12:-multiple}
+CHANNELS=${13:-first}
+FREEZE=${14:-on}
+BS=${15:0}
+LOG_DIR=${16:-inductor_log}
+HF_TOKEN=${17:-hf_xxx}
+BACKEND=${18:-inductor}
+PERF_RATIO=${19:-1.1}
+EXTRA=${20}
 
 export HUGGING_FACE_HUB_TOKEN=${HF_TOKEN}
 
@@ -36,7 +37,7 @@ if [ "$SCENARIO" == "performance" ] && [ "$KIND" == "drop" ]; then
     echo "Expected performance: $expected_perf s"
 fi
 chmod +x bisect_run_test.sh
-git reset --hard HEAD && git checkout main && git pull && git checkout ${START_COMMIT}
+git reset --hard HEAD && git checkout ${TORCH_BRANCH} && git pull && git checkout ${START_COMMIT}
 git bisect start ${START_COMMIT} ${END_COMMIT}
 git bisect run ./bisect_run_test.sh $SUITE $MODEL $MODE $PRECISION $SHAPE $WRAPPER $SCENARIO $KIND $THREADS $CHANNELS $FREEZE 0 $expected_perf $BACKEND $PERF_RATIO $EXTRA 2>&1 | tee ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log
 git bisect reset
