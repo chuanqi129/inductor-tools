b5a87de510e7c9a8bdb2b81bde6953c3e61c4b3c
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-01-25 14:04:12 +0800

    use getParameters

--- groovy/inductor_job_compare.groovy
+++ groovy/inductor_job_compare.groovy
@@ -25,15 +25,6 @@ if ('debug' in params) {
 }
 echo "debug: $debug"
 
-debug_mail = 'yudong.si@intel.com'
-if ('debug_mail' in params) {
-    echo "debug_mail in params"
-    if (params.debug_mail != '') {
-        debug_mail = params.debug_mail
-    }
-}
-echo "debug_mail: $debug_mail"
-
 cppwp_gm = 'False'
 if ('cppwp_gm' in params) {
     echo "cppwp_gm in params"
@@ -79,11 +70,11 @@ if ('st_end' in params) {
 }
 echo "st_end: $st_end"
 
-target_job = 'inductor_aws_regular_cppwrapper'
+env.target_job = 'inductor_aws_regular_cppwrapper'
 if ('target_job' in params) {
     echo "target_job in params"
     if (params.target_job != '') {
-        target_job = params.target_job
+        env.target_job = params.target_job
     }
 }
 echo "target_job: $target_job"
@@ -114,6 +105,34 @@ if ('refer_job_selector' in params) {
     }
 }
 echo "refer_job_selector: $refer_job_selector"
+
+env.auto_guilty_commit_search = 'false'
+if ('auto_guilty_commit_search' in params) {
+    echo "auto_guilty_commit_search in params"
+    if (params.auto_guilty_commit_search != '') {
+        env.auto_guilty_commit_search = params.auto_guilty_commit_search
+    }
+}
+echo "auto_guilty_commit_search: $auto_guilty_commit_search"
+
+import hudson.model.ParametersAction
+
+def getParams(String project, String buildNumber){
+
+  def params=[]
+
+  Jenkins.instance.getItem(project).getBuild(buildNumber).getActions(ParametersAction)
+  .each { action ->
+    action.getParameters().each {
+      params << it
+    }
+  }
+
+  return params
+}
+
+env._precision = getParams("$target_job", "$target_job_selector")
+echo "${_precision}"
 
 env._precision = "$precision"
 env._target_job = "$target_job"
@@ -153,7 +172,7 @@ node(NODE_LABEL){
         sh '''
         #!/usr/bin/env bash
         cd ${WORKSPACE} && rm inductor_log/*.html && rm inductor_log/*.xlsx && mkdir -p ${_refer_job}_${_refer_sc} && mv inductor_log ${_refer_job}_${_refer_sc}
-        '''        
+        '''
     }
     stage("report"){
         sh '''
@@ -172,6 +191,39 @@ node(NODE_LABEL){
         mv ${_target_job}_${_target_sc}/inductor_log/*.xlsx ./ && mv ${_target_job}_${_target_sc}/inductor_log/*.html ./ && rm -rf ${_refer_job}_${_refer_sc} && rm -rf ${_target_job}_${_target_sc}
         '''
         archiveArtifacts  "*.xlsx, *.html"
+    }
+
+    stage("trigger Auto Guilty Commit Search job"){
+        // TODO: refer_build
+        if ("${auto_guilty_commit_search}" == "true") {
+            if (!("${JOB_NAME}" =~ "inductor_job_result_compare")) {
+                env.target_job = "${JOB_NAME}"
+            }
+            env.shape = "${target_job}" =~ "ds" ? "dynamic" : "static";
+            env.WRAPPER = "${target_job}" =~ "cppwrapper" ? "cpp" : "default";
+            env.instance_name = "${_precision}" =~ "float32" ? "icx-guilty-search" : "spr-guilty-search";
+            // TODO: THREADS, suite, model, scenario, kind, TORCH_START_COMMIT, TORCH_END_COMMIT
+            // if ("${target_job}" =~ "ds") {
+            //     env.shape = "dynamic"
+            // } else {
+            //     env.shape = "static"
+            // }
+            def guilty_commit_search_job = build job: 'inductor_aws_guilty_commit_search', propagate: false, parameters: [
+                [$class: 'StringParameterValue', name: 'default_mail', value: "${debug_mail}"],
+                [$class: 'StringParameterValue', name: 'precision', value: "${_precision}"],                
+                [$class: 'StringParameterValue', name: 'shape', value: "${shape}"],
+                [$class: 'StringParameterValue', name: 'TORCH_BRANCH', value: "${TORCH_BRANCH}"],
+                [$class: 'StringParameterValue', name: 'THREADS', value: "${THREADS}"],
+                [$class: 'StringParameterValue', name: 'WRAPPER', value: "${WRAPPER}"],
+                [$class: 'StringParameterValue', name: 'instance_name', value: "${instance_name}"],
+                [$class: 'StringParameterValue', name: 'suite', value: "${suite}"],
+                [$class: 'StringParameterValue', name: 'model', value: "${model}"],
+                [$class: 'StringParameterValue', name: 'scenario', value: "${scenario}"],
+                [$class: 'StringParameterValue', name: 'kind', value: "${kind}"],
+                [$class: 'StringParameterValue', name: 'TORCH_START_COMMIT', value: "${TORCH_START_COMMIT}"],
+                [$class: 'StringParameterValue', name: 'TORCH_END_COMMIT', value: "${TORCH_END_COMMIT}"],
+            ]
+        }
     }
 
     stage("Email"){
