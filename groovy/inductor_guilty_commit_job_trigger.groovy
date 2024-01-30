daf6063d63f2d99c66a0b933c7423e6ef675b112
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-01-30 15:58:25 +0800

    debug

--- groovy/inductor_guilty_commit_job_trigger.groovy
+++ groovy/inductor_guilty_commit_job_trigger.groovy
@@ -12,25 +12,23 @@ node("weizhuoz-mlt-ace"){
                 if (elem['precision'] == "amp") {
                     instance_name = 'spr-guilty-search'
                 }
-                print(elem['precision'])
-                println(instance_name)
-                //job_list["Job_${i}"] = {
-                //    guilty_commit_search_job = build job: 'inductor_aws_guilty_commit_search_zwz', propagate: false, parameters: [
-                //        [$class: 'StringParameterValue', name: 'default_mail', value: default_mail],
-                //        [$class: 'StringParameterValue', name: 'precision', value: elem['precision']],                
-                //        [$class: 'StringParameterValue', name: 'shape', value: shape],
-                //        [$class: 'StringParameterValue', name: 'WRAPPER', value: "default"],
-                //        [$class: 'StringParameterValue', name: 'TORCH_BRANCH', value: TORCH_BRANCH],
-                //        [$class: 'StringParameterValue', name: 'THREADS', value: elem['thread']],
-                //        [$class: 'StringParameterValue', name: 'instance_name', value: instance_name],
-                //        [$class: 'StringParameterValue', name: 'suite', value: elem['suite']],
-                //        [$class: 'StringParameterValue', name: 'model', value: elem['name']],
-                //        [$class: 'StringParameterValue', name: 'scenario', value: elem['scenario']],
-                //        [$class: 'StringParameterValue', name: 'kind', value: elem['kind']],
-                //        [$class: 'StringParameterValue', name: 'TORCH_START_COMMIT', value: TORCH_START_COMMIT],
-                //        [$class: 'StringParameterValue', name: 'TORCH_END_COMMIT', value: TORCH_END_COMMIT],
-                //    ]
-                //}
+                job_list["Job_${i}"] = {
+                    guilty_commit_search_job = build job: 'inductor_aws_guilty_commit_search_zwz', propagate: false, parameters: [
+                        [$class: 'StringParameterValue', name: 'default_mail', value: default_mail],
+                        [$class: 'StringParameterValue', name: 'precision', value: elem['precision']],                
+                        [$class: 'StringParameterValue', name: 'shape', value: shape],
+                        [$class: 'StringParameterValue', name: 'WRAPPER', value: "default"],
+                        [$class: 'StringParameterValue', name: 'TORCH_BRANCH', value: TORCH_BRANCH],
+                        [$class: 'StringParameterValue', name: 'THREADS', value: elem['thread']],
+                        [$class: 'StringParameterValue', name: 'instance_name', value: instance_name],
+                        [$class: 'StringParameterValue', name: 'suite', value: elem['suite']],
+                        [$class: 'StringParameterValue', name: 'model', value: elem['name']],
+                        [$class: 'StringParameterValue', name: 'scenario', value: elem['scenario']],
+                        [$class: 'StringParameterValue', name: 'kind', value: elem['kind']],
+                        [$class: 'StringParameterValue', name: 'TORCH_START_COMMIT', value: TORCH_START_COMMIT],
+                        [$class: 'StringParameterValue', name: 'TORCH_END_COMMIT', value: TORCH_END_COMMIT],
+                    ]
+                }
             }
         }
         parallel job_list
