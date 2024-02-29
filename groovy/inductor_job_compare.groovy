1fb36e8b27f293d9aa5cbb02109dacc5f28671c9
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-02-29 17:22:42 +0800

    fix groovy param bug

--- groovy/inductor_job_compare.groovy
+++ groovy/inductor_job_compare.groovy
@@ -198,6 +198,11 @@ node(NODE_LABEL){
         '''        
     }
     stage("report"){
+        def params = getUpstreamParameters(target_job, target_job_selector)
+        def shape = params.get('shape')
+        def wrapper = params.get('WRAPPER')
+        def torch_repo = params.get('TORCH_REPO')
+        def torch_branch = params.get('TORCH_BRANCH')
         sh '''
         #!/usr/bin/env bash
         if [ ${_NODE} == 'mlp-spr-04.sh.intel.com' ];then
@@ -206,11 +211,6 @@ node(NODE_LABEL){
         # Install dependencies
         pip install scipy datacompy PyGithub styleframe pandas bs4 requests
         cp scripts/modelbench/report.py ${WORKSPACE}
-        def params = getUpstreamParameters(target_job, target_job_selector)
-        def shape = params.get('shape')
-        def wrapper = params.get('WRAPPER')
-        def torch_repo = params.get('TORCH_REPO')
-        def torch_branch = params.get('TORCH_BRANCH')
         if [ ${_cppwp_gm} == 'True' ];then
             python report.py -r ${_refer_job}_${_refer_sc} -t ${_target_job}_${_target_sc} -m all --md_off --url ${BUILD_URL} --precision ${_precision} --cppwrapper_gm --mt_interval_start ${_mt_start} --mt_interval_end ${_mt_end} --st_interval_start ${_st_start} --st_interval_end ${_st_end} --suite ${_suite} --infer_or_train ${_infer_or_train} --shape ${shape} --wrapper ${wrapper} --torch_repo ${torch_repo} --torch_branch ${torch_branch}
         else
