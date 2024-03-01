8b000c07d6f5cbbc5e3572d299bb697327406347
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-03-01 13:58:23 +0800

    reformat job-list

--- groovy/inductor_guilty_commit_job_trigger.groovy
+++ groovy/inductor_guilty_commit_job_trigger.groovy
@@ -10,6 +10,10 @@ node(NODE_LABEL){
     }
 
     stage('Trigger Guilty Commit Job'){
+        sh'''
+            touch ${WORKSPACE}/inductor_guilty_commit_search_summary.csv
+            echo "suite,name,scenario,thread,kind,precision,shape,wrapper,guilty_commit,job_link" >> ${WORKSPACE}/inductor_guilty_commit_search_summary.csv
+        '''
         def common_info_dict = readJSON file: 'guilty_commit_search_common_info.json'
         def model_list_lines = readJSON file: 'guilty_commit_search_model_list.json'
 
@@ -23,79 +27,60 @@ node(NODE_LABEL){
                     instance_name = 'spr-guilty-search'
                 }
 
-                job_parameters = [
-                    string(name: 'default_mail', value: default_mail),
-                    string(name: 'precision', value: elem['precision']),
-                    string(name: 'shape', value: common_info_dict['shape']),
-                    string(name: 'WRAPPER', value: common_info_dict['wrapper']),
-                    string(name: 'TORCH_REPO', value: common_info_dict['torch_repo']),
-                    string(name: 'TORCH_BRANCH', value: common_info_dict['torch_branch']),
-                    string(name: 'THREADS', value: elem['thread']),
-                    string(name: 'instance_name', value: instance_name),
-                    string(name: 'suite', value: elem['suite']),
-                    string(name: 'model', value: elem['name']),
-                    string(name: 'scenario', value: elem['scenario']),
-                    string(name: 'kind', value: elem['kind']),
-                    string(name: 'TORCH_START_COMMIT', value: common_info_dict['start_commit']),
-                    string(name: 'TORCH_END_COMMIT', value: common_info_dict['end_commit']),
-                ]
+                def suite = elem['suite']
+                def model = elem['name']
+                def scenario = elem['scenario']
+                def thread = elem['thread']
+                def kind = elem['kind']
+                def precision = elem['precision']
+                def shape = common_info_dict['shape']
+                def wrapper = common_info_dict['wrapper']
 
-                job_list["job_${i}"] = {build propagate: false, job: guilty_commit_search_job_name, parameters: [
-                    string(name: 'default_mail', value: default_mail),
-                    string(name: 'precision', value: elem['precision']),
-                    string(name: 'shape', value: common_info_dict['shape']),
-                    string(name: 'WRAPPER', value: common_info_dict['wrapper']),
-                    string(name: 'TORCH_REPO', value: common_info_dict['torch_repo']),
-                    string(name: 'TORCH_BRANCH', value: common_info_dict['torch_branch']),
-                    string(name: 'THREADS', value: elem['thread']),
-                    string(name: 'instance_name', value: instance_name),
-                    string(name: 'suite', value: elem['suite']),
-                    string(name: 'model', value: elem['name']),
-                    string(name: 'scenario', value: elem['scenario']),
-                    string(name: 'kind', value: elem['kind']),
-                    string(name: 'TORCH_START_COMMIT', value: common_info_dict['start_commit']),
-                    string(name: 'TORCH_END_COMMIT', value: common_info_dict['end_commit']),
-                ]}
+                job_list["${suite}_${model}_${precision}_${kind}_${thread}"] = {
+                    def job_parameters = [
+                        string(name: 'default_mail', value: default_mail),
+                        string(name: 'suite', value: suite),
+                        string(name: 'model', value: model),
+                        string(name: 'scenario', value: scenario),
+                        string(name: 'THREADS', value: thread),
+                        string(name: 'kind', value: kind),
+                        string(name: 'precision', value: precision),
+                        string(name: 'shape', value: shape),
+                        string(name: 'WRAPPER', value: wrapper),
+                        string(name: 'instance_name', value: instance_name),
+                        string(name: 'TORCH_REPO', value: common_info_dict['torch_repo']),
+                        string(name: 'TORCH_BRANCH', value: common_info_dict['torch_branch']),
+                        string(name: 'TORCH_START_COMMIT', value: common_info_dict['start_commit']),
+                        string(name: 'TORCH_END_COMMIT', value: common_info_dict['end_commit']),
+                    ]
+
+                    def guilty_commit_job = build propagate: false,
+                        job: guilty_commit_search_job_name, parameters: job_parameters
+                    
+                    def cur_job_number = guilty_commit_job.getNumber()
+                    def cur_job_url = guilty_commit_job.getAbsoluteUrl()
+                    def cur_job_duration = guilty_commit_job.getDurationString()
+
+                    copyArtifacts(
+                        projectName: guilty_commit_search_job_name,
+                        selector: specific("${cur_job_number}"),
+                        target: "inductor_guilty_commit_search/${cur_job_number}"
+                    )
+
+                    if (fileExists("${WORKSPACE}/inductor_guilty_commit_search/${cur_job_number}/*/inductor_log/guilty_commit.log") == true){
+                        sh'''
+                            guilty_commit=`cat ${WORKSPACE}/inductor_guilty_commit_search/${cur_job_number}/*/inductor_log/guilty_commit.log | head -1`
+                            echo "${suite},${name},${scenario},${thread},${kind},${precision},${shape},${wrapper},${guilty_commit},${cur_job_url}" >> ${WORKSPACE}/inductor_guilty_commit_search_summary.csv
+                        '''
+                    } else {
+                        sh'''
+                            echo "${suite},${name},${scenario},${thread},${kind},${precision},${shape},${wrapper},fake,${cur_job_url}" >> ${WORKSPACE}/inductor_guilty_commit_search_summary.csv
+                        '''
+                    }
             }
-        }
+        } // for
         parallel job_list
-    }
-
-    stage('Collecting and Analyzing report'){
-        sh'''
-            touch ${WORKSPACE}/inductor_guilty_commit_search_summary.csv
-            echo "suite,name,scenario,thread,kind,precision,guilty_commit,job_link" >> ${WORKSPACE}/inductor_guilty_commit_search_summary.csv
-        '''
-        for (key in job_list) {
-            job_variables = job_list[key].getBuildVariables()
-            suite = job_variables['suite']
-            model = job_variables['model']
-            scenario = job_variables['scenario']
-            thread = job_variables['THREADS']
-            kind = job_variables['kind']
-            precision = job_variables['precision']
-
-            cur_job_number = job_list[key].getNumber()
-            cur_job_url = job_list[key].getAbsoluteUrl()
-            cur_job_duration = job_list[key].getDurationString()
-
-            copyArtifacts(
-                projectName: guilty_commit_search_job_name,
-                selector: specific("${cur_job_number}"),
-                target: "inductor_guilty_commit_search/${cur_job_number}"
-            )
-            if (fileExists("${WORKSPACE}/inductor_guilty_commit_search/${cur_job_number}/*/inductor_log/guilty_commit.log") == true){
-                sh'''
-                    guilty_commit=`cat ${WORKSPACE}/inductor_guilty_commit_search/${cur_job_number}/*/inductor_log/guilty_commit.log | head -1`
-                    echo "${suite},${name},${scenario},${thread},${kind},${precision},${guilty_commit},${cur_job_url}" >> ${WORKSPACE}/inductor_guilty_commit_search_summary.csv
-                '''
-            } else {
-                sh'''
-                    echo "${suite},${name},${scenario},${thread},${kind},${precision},fake,${cur_job_url}" >> ${WORKSPACE}/inductor_guilty_commit_search_summary.csv
-                '''
-            }
-        }
-    }
+    } // stage trigger
 
     // stage('Email') {
     //     emailext(
