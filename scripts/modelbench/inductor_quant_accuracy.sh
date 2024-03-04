fe72bdd2fa331b49ad906daa3cbd4fa1e69f7dd8
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-03-04 09:47:15 +0800

    Update inductor_quant_accuracy.sh

--- scripts/modelbench/inductor_quant_accuracy.sh
+++ scripts/modelbench/inductor_quant_accuracy.sh
@@ -1,5 +1,5 @@
-# export LD_PRELOAD=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libiomp5.so:${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libjemalloc.so
-# export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:-1,muzzy_decay_ms:-1"
+export LD_PRELOAD=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libiomp5.so:${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libjemalloc.so
+export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:-1,muzzy_decay_ms:-1"
 LOG_DIR=${1:-inductor_log}
 cd ../benchmark
 mkdir -p $LOG_DIR
