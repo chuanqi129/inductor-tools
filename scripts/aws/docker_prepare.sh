8f6cfb826dcda9c567b3088e4bf4fc4d5a91e782
mengfeil <mengfei.li@intel.com>
2024-04-01 18:51:04 +0800

    remove TAS branch

--- scripts/aws/docker_prepare.sh
+++ scripts/aws/docker_prepare.sh
@@ -2,6 +2,7 @@ set +e
 
 # docker setup
 sudo apt update -y
+sudo apt install -y openjdk-11-jre-headless # java for jenkins
 sudo apt install docker.io docker-buildx -y && sudo usermod -aG docker ${USER}
 if [[ ! $(groups "${USER}" | grep -q docker) ]]; then
   sudo groupadd docker
