fd018ce10cf39600849c1db7d51bd4411e12f328
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-05-06 23:04:39 +0800

    Update bisect_search_quant.sh

--- scripts/modelbench/bisect_search_quant.sh
+++ scripts/modelbench/bisect_search_quant.sh
@@ -58,10 +58,10 @@ if [ "$SCENARIO" == "performance" ] && [ "$KIND" == "drop" ]; then
         exit 0
     fi
 fi
-chmod +x bisect_run_test.sh
+chmod +x bisect_run_test_quant.sh
 git reset --hard HEAD && git checkout ${TORCH_BRANCH} && git pull && git checkout ${START_COMMIT}
 git bisect start ${START_COMMIT} ${END_COMMIT}
-git bisect run ./bisect_run_test.sh $SUITE $MODEL $MODE $PRECISION $SHAPE $WRAPPER $SCENARIO $KIND $THREADS $CHANNELS $FREEZE 0 $expected_perf $BACKEND $PERF_RATIO $EXTRA 2>&1 | tee ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log
+git bisect run ./bisect_run_test_quant.sh $SUITE $MODEL $MODE $PRECISION $SHAPE $WRAPPER $SCENARIO $KIND $THREADS $CHANNELS $FREEZE 0 $expected_perf $BACKEND $PERF_RATIO $EXTRA 2>&1 | tee ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log
 git bisect reset
 cat ${LOG_DIR}/${SUITE}-${MODEL}-${MODE}-${PRECISION}-${SHAPE}-${WRAPPER}-${SCENARIO}-${THREADS}-${KIND}_guilty_commit.log | grep "is the first bad commit" | awk '{ print $1 }' > ${LOG_DIR}/guitly_commit.log
 echo "Start commit: ${START_COMMIT}" >> ${LOG_DIR}/guitly_commit.log
