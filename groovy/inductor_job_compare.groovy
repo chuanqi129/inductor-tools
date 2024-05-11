91b99060c2b18cdfe6d79942b4ec5dd0f6cc34e9
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-05-11 08:52:12 +0800

    fix THREADS param in job_compare bug

--- groovy/inductor_job_compare.groovy
+++ groovy/inductor_job_compare.groovy
@@ -216,6 +216,7 @@ node(NODE_LABEL){
             env._suite = params.get('suite')
             env._precision = params.get('precision')
             env.backend = params.get('backend')
+            env.thread = params.get('THREADS')
 
             def ref_params = getUpstreamParameters(_refer_job, _refer_sc)
             env.ref_backend = ref_params.get('backend')
@@ -236,9 +237,9 @@ node(NODE_LABEL){
             pip install scipy datacompy PyGithub styleframe pandas bs4 requests
             cp scripts/modelbench/report.py ${WORKSPACE}
             if [ ${_cppwp_gm} == 'True' ];then
-                python report.py -r ${_refer_job}_${_refer_sc} -t ${_target_job}_${_target_sc} -m all --md_off --url ${BUILD_URL} --precision ${_precision} --cppwrapper_gm --mt_interval_start ${_mt_start} --mt_interval_end ${_mt_end} --st_interval_start ${_st_start} --st_interval_end ${_st_end} --suite ${_suite} --infer_or_train ${_infer_or_train} --shape ${shape} --wrapper ${wrapper} --torch_repo ${torch_repo} --torch_branch ${torch_branch}  --backend ${backend} --threshold ${threshold} --ref_backend ${ref_backend} 
+                python report.py -r ${_refer_job}_${_refer_sc} -t ${_target_job}_${_target_sc} -m ${thread} --md_off --url ${BUILD_URL} --precision ${_precision} --cppwrapper_gm --mt_interval_start ${_mt_start} --mt_interval_end ${_mt_end} --st_interval_start ${_st_start} --st_interval_end ${_st_end} --suite ${_suite} --infer_or_train ${_infer_or_train} --shape ${shape} --wrapper ${wrapper} --torch_repo ${torch_repo} --torch_branch ${torch_branch}  --backend ${backend} --threshold ${threshold} --ref_backend ${ref_backend} 
             else
-                python report.py -r ${_refer_job}_${_refer_sc} -t ${_target_job}_${_target_sc} -m all --md_off --url ${BUILD_URL} --precision ${_precision} --suite ${_suite} --infer_or_train ${_infer_or_train} --shape ${shape} --wrapper ${wrapper} --shape ${shape} --wrapper ${wrapper} --torch_repo ${torch_repo} --torch_branch ${torch_branch} --backend ${backend} --threshold ${threshold} --ref_backend ${ref_backend} 
+                python report.py -r ${_refer_job}_${_refer_sc} -t ${_target_job}_${_target_sc} -m ${thread} --md_off --url ${BUILD_URL} --precision ${_precision} --suite ${_suite} --infer_or_train ${_infer_or_train} --shape ${shape} --wrapper ${wrapper} --shape ${shape} --wrapper ${wrapper} --torch_repo ${torch_repo} --torch_branch ${torch_branch} --backend ${backend} --threshold ${threshold} --ref_backend ${ref_backend} 
             fi
             mv ${_target_job}_${_target_sc}/inductor_log/*.xlsx ./ && mv ${_target_job}_${_target_sc}/inductor_log/*.html ./
             '''
