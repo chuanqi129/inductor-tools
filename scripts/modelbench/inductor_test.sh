830329b3db8839f717f600410cccfcea40aaf553
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-05-23 22:52:14 +0800

    Update inductor_test.sh

--- scripts/modelbench/inductor_test.sh
+++ scripts/modelbench/inductor_test.sh
@@ -65,8 +65,10 @@ if [[ $SUITE == "all" ]]; then
 else
     SUITE="--suites=${SUITE}"
 fi
-export TORCH_COMPILE_DEBUG=1
-export TORCH_LOGS="+schedule,+inductor,+output_code"
+#export TORCH_COMPILE_DEBUG=1
+#export TORCH_LOGS="+schedule,+inductor,+output_code"
+export _USE_ONEDNN_GEMM=1
+export ONEDNN_VERBOSE=1
 # multi-threads
 multi_threads_test() {
     CORES=$(lscpu | grep Core | awk '{print $4}')
