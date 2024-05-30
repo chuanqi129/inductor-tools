540cd9a96b6c04f1e1b19ae1fd1c42f826dca8a9
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-05-30 15:19:24 +0800

    fix bug

--- groovy/inductor_locally_benchmark.groovy
+++ groovy/inductor_locally_benchmark.groovy
@@ -115,9 +115,8 @@ node(NODE_LABEL){
                         DOCKER_TAG="${commit_date}-${bref_commit}"
 
                         docker login ccr-registry.caas.intel.com -u $USERNAME -p $PASSWORD
-                        docker manifest inspect ${DOCKER_IMAGE_NAMESPACE}:${DOCKER_TAG} > /dev/null 2>&1
-                        status=${PIPESTATUS[0]}
-                        if [ "$status" != "0" ];then
+                        docker_img_status=`docker manifest inspect ${DOCKER_IMAGE_NAMESPACE}:${DOCKER_TAG}` || true
+                        if [ -n "${docker_img_status}" ];then
                             # build docker image, because the target images does not exist
                             DOCKER_BUILDKIT=1 docker build \
                                 --no-cache \
