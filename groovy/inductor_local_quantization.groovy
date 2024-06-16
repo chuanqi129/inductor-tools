8e1ad33160ad94d8a1c7f8a6120e4d71486de42a
zengxian <xiangdong.zeng@intel.com>
2024-06-16 08:18:35 -0700

    enable local quant

--- groovy/inductor_local_quantization.groovy
+++ groovy/inductor_local_quantization.groovy
@@ -519,7 +519,7 @@ node(NODE_LABEL){
             #!/usr/bin/env bash
             cd ${WORKSPACE}
             cp scripts/modelbench/quant/report_quant_perf.py ${WORKSPACE}
-            conda env list
+            source activate base
             python3 report_quant_perf.py -r refer -t ${target} --url ${BUILD_URL}
             rm -rf refer  
             '''
