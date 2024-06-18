c02b0e5e3825bdb81b285b8691299468e44de4c1
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-06-18 16:00:27 +0800

    skip image build and push if image existed

--- groovy/inductor_images.groovy
+++ groovy/inductor_images.groovy
@@ -163,7 +163,7 @@ node(NODE_LABEL){
                 cp docker/Dockerfile ./
                 DOCKER_BUILDKIT=1 docker build --no-cache --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg PT_REPO=${PT_REPO} --build-arg PT_COMMIT=${PT_COMMIT} --build-arg TORCH_VISION_COMMIT=${TORCH_VISION_COMMIT} --build-arg TORCH_DATA_COMMIT=${TORCH_DATA_COMMIT} --build-arg TORCH_TEXT_COMMIT=${TORCH_TEXT_COMMIT} --build-arg TORCH_AUDIO_COMMIT=${TORCH_AUDIO_COMMIT} --build-arg TORCH_BENCH_COMMIT=${TORCH_BENCH_COMMIT} --build-arg BENCH_COMMIT=${BENCH_COMMIT} --build-arg HF_HUB_TOKEN=${HF_TOKEN} -t ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag} -f Dockerfile --target image .
             else
-                docker pull ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag}
+                echo "ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag} existed, skip build image"
             fi
             '''
         }
@@ -174,7 +174,12 @@ node(NODE_LABEL){
             echo 'push image......'
             sh '''
             #!/usr/bin/env bash
-            docker push ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag}
+            docker_img_status=`docker manifest inspect ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag}` || true
+            if [ -z "${docker_img_status}" ];then
+                docker push ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag}
+            else
+                echo "ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag} existed, skip push image"
+            fi
             '''
         }
     }
