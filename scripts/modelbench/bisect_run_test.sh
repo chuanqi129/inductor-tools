8320ad98540cf8e79fed28c3e5e130fd17e58f2c
WeizhuoZhang-intel <weizhuo.zhang@intel.com>
2024-06-25 16:40:00 +0800

    Local guilty commit search (#98)
    * Modify Dockerfile conda to conda-forge
    
    * finish draft for local
    
    * add logic for report only
    
    * remove jenkins credentials
    
    * fix docker clean
    
    * fix not empty check
    
    * fix
    
    * fix bug
    
    * fix empty str bug
    
    * fix path bug
    
    * add file check
    
    * add chmod 777 to target log folder
    
    * modify LOG_DIR
    
    * fix path
    
    * fix log bug
    
    * fix archive bug
    
    * fix bug
    
    * fix single suite report bug
    
    * add check scm for report-only
    
    * fix log bug
    
    * merge main Dockerfile
    
    * revert report.py change
    
    * Revert "revert report.py change"
    
    This reverts commit f7e0785ccf29b11773049e7cff176e3344d9c3c6.
    
    * fix log dir bug
    
    * add docker prepare for centos
    
    * add HF_TOKEN to env
    
    * use independent inductor image job
    
    * fix syntax error
    
    * add image check on image jobs
    
    * fix bug
    
    * remove other code
    
    * comment out OMP_NUM_THREADS for multi thread test
    
    * add build_image
    
    * fix commit
    
    * add us node clone
    
    * fix
    
    * add retry
    
    * add node_label allocated by precision
    
    * add retry
    
    * add retry on sleep
    
    * draft for local guilty commit search
    
    * fix script bug
    
    * fix email file exist bug
    
    * rename inductor images job
    
    * guilty commit image build job use locally
    
    * gnr local tas use ubuntu node
    
    * add target folder for guilty commit test
    
    * add retry for guilty commit job trigger
    
    * add benchmark pipeline
    
    * add retry for image_build_job
    
    * skip image build and push if image existed
    
    * use inductor-icx-local-tas for TAS test
    
    * rename NODE_LABEL to report_node for pipeline
    
    * Weizhuoz/use one node pipeline (#101)
    
    * For pipeline job, baseline and target using same machine
    
    * use debug benchmark job
    
    * Revert "use debug benchmark job"
    
    This reverts commit 05fd966653a98c13e004f09075113c78bbc26346.

--- scripts/modelbench/bisect_run_test.sh
+++ scripts/modelbench/bisect_run_test.sh
@@ -21,10 +21,10 @@ prepare_test() {
     rm -rf /tmp/*
     git reset --hard HEAD && git submodule sync && git submodule update --init --recursive
     python setup.py clean && python setup.py develop && cd .. && \
-    rm -rf vision && git clone -b main https://github.com/pytorch/vision.git && cd vision && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/vision.txt` && pip uninstall torchvision -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
-    rm -rf data && git clone -b main https://github.com/pytorch/data.git && cd data && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/data.txt`  && pip uninstall torchdata -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
-    rm -rf text && git clone -b main https://github.com/pytorch/text.git && cd text && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/text.txt` && pip uninstall torchtext -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
-    rm -rf audio && git clone -b main https://github.com/pytorch/audio.git && cd audio && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/audio.txt` && pip uninstall torchaudio -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    cd vision && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/vision.txt` && pip uninstall torchvision -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    cd data && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/data.txt`  && pip uninstall torchdata -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    cd text && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/text.txt` && pip uninstall torchtext -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    cd audio && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/audio.txt` && pip uninstall torchaudio -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
     export TRANSFORMERS_COMMIT=`cat /workspace/pytorch/.ci/docker/ci_commit_pins/huggingface.txt` && pip install --force-reinstall git+https://github.com/huggingface/transformers@${TRANSFORMERS_COMMIT}
     cd benchmark && git checkout main && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/torchbench.txt` && cd /workspace/pytorch
 }
--- scripts/modelbench/bisect_search.sh
+++ scripts/modelbench/bisect_search.sh
@@ -53,11 +53,10 @@ if [ "$SCENARIO" == "performance" ] && ([ "$KIND" == "drop" ] || [ "$KIND" == "i
     rm -rf /tmp/*
     git reset --hard HEAD && git checkout ${START_COMMIT} && git submodule sync && git submodule update --init --recursive
     python setup.py clean && python setup.py develop && cd .. && \
-    rm -rf vision && git clone -b main https://github.com/pytorch/vision.git && cd vision && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/vision.txt` && pip uninstall torchvision -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
-    rm -rf data && git clone -b main https://github.com/pytorch/data.git && cd data && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/data.txt`  && pip uninstall torchdata -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
-    rm -rf text && git clone -b main https://github.com/pytorch/text.git && cd text && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/text.txt` && pip uninstall torchtext -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
-    rm -rf audio && git clone -b main https://github.com/pytorch/audio.git && cd audio && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/audio.txt` && pip uninstall torchaudio -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
-    rm -rf transformers && git clone -b main https://github.com/huggingface/transformers.git && cd transformers && git checkout `cat /workspace/pytorch/.ci/docker/ci_commit_pins/huggingface.txt` && pip uninstall transformers -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    cd vision && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/vision.txt` && pip uninstall torchvision -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    cd data && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/data.txt`  && pip uninstall torchdata -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    cd text && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/text.txt` && pip uninstall torchtext -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
+    cd audio && git checkout `cat /workspace/pytorch/.github/ci_commit_pins/audio.txt` && pip uninstall torchaudio -y && python setup.py bdist_wheel && pip install dist/*.whl && cd .. && \
     export TRANSFORMERS_COMMIT=`cat /workspace/pytorch/.ci/docker/ci_commit_pins/huggingface.txt` && pip install --force-reinstall git+https://github.com/huggingface/transformers@${TRANSFORMERS_COMMIT} && cd /workspace/pytorch
     detected_value=$(bash ./inductor_single_run.sh $THREADS $MODE $SCENARIO $SUITE $MODEL $PRECISION $CHANNELS $SHAPE $WRAPPER $BS $FREEZE $BACKEND | tail -n 1 | awk -F, '{print $5}')
     current_perf=$(echo $detected_value | awk '{ printf "%.5f", $1/1000 }')
