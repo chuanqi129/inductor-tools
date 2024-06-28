bd8f1a139c5c496741ef675aa5b6d09af7fcb970
WeizhuoZhang-intel <weizhuo.zhang@intel.com>
2024-06-28 11:36:53 +0800

    use existed condarc (#104)

--- groovy/inductor_images.groovy
+++ groovy/inductor_images.groovy
@@ -161,6 +161,7 @@ node(NODE_LABEL){
             docker_img_status=`docker manifest inspect ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag}` || true
             if [ -z "${docker_img_status}" ];then
                 cp docker/Dockerfile ./
+                cp docker/condarc ./
                 DOCKER_BUILDKIT=1 docker build --no-cache --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg PT_REPO=${PT_REPO} --build-arg PT_COMMIT=${PT_COMMIT} --build-arg TORCH_VISION_COMMIT=${TORCH_VISION_COMMIT} --build-arg TORCH_DATA_COMMIT=${TORCH_DATA_COMMIT} --build-arg TORCH_TEXT_COMMIT=${TORCH_TEXT_COMMIT} --build-arg TORCH_AUDIO_COMMIT=${TORCH_AUDIO_COMMIT} --build-arg TORCH_BENCH_COMMIT=${TORCH_BENCH_COMMIT} --build-arg BENCH_COMMIT=${BENCH_COMMIT} --build-arg HF_HUB_TOKEN=${HF_TOKEN} -t ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag} -f Dockerfile --target image .
             else
                 echo "ccr-registry.caas.intel.com/pytorch/pt_inductor:${tag} existed, skip build image"
