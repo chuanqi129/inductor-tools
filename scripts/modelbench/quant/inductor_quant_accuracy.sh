00a6fbb175b7d062a38397b342be25b252894d2c
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-07-13 17:51:16 +0800

    Update inductor_quant_accuracy.sh

--- scripts/modelbench/quant/inductor_quant_accuracy.sh
+++ scripts/modelbench/quant/inductor_quant_accuracy.sh
@@ -5,14 +5,16 @@ cd ../benchmark
 mkdir -p $LOG_DIR
 mkdir inductor_quant_acc/
 pip install networkx==2.8
+export TORCH_COMPILE_DEBUG=1
+export TORCH_LOGS="+schedule,+inductor,+output_code"
 
 python inductor_quant_acc.py 2>&1 |& tee "./inductor_quant_acc_ptq.log"
 mv ./inductor_quant_acc_ptq.log inductor_quant_acc/
-TORCHINDUCTOR_CPP_WRAPPER=1 python inductor_quant_acc.py --cpp_wrapper 2>&1 |& tee "./inductor_quant_acc_ptq_cpp_wrapper.log"
-mv ./inductor_quant_acc_ptq_cpp_wrapper.log inductor_quant_acc/
-python inductor_quant_acc.py  --is_qat 2>&1 |& tee "./inductor_quant_acc_qat.log"
-mv ./inductor_quant_acc_qat.log inductor_quant_acc/
-python inductor_quant_acc.py --is_fp32 2>&1 |& tee "./inductor_quant_acc_fp32.log"
-mv ./inductor_quant_acc_fp32.log inductor_quant_acc/
+# TORCHINDUCTOR_CPP_WRAPPER=1 python inductor_quant_acc.py --cpp_wrapper 2>&1 |& tee "./inductor_quant_acc_ptq_cpp_wrapper.log"
+# mv ./inductor_quant_acc_ptq_cpp_wrapper.log inductor_quant_acc/
+# python inductor_quant_acc.py  --is_qat 2>&1 |& tee "./inductor_quant_acc_qat.log"
+# mv ./inductor_quant_acc_qat.log inductor_quant_acc/
+# python inductor_quant_acc.py --is_fp32 2>&1 |& tee "./inductor_quant_acc_fp32.log"
+# mv ./inductor_quant_acc_fp32.log inductor_quant_acc/
 
 mv inductor_quant_acc ../pytorch/$LOG_DIR/
