05917b0f38ba127df5d2a878a7b29fa84ce0eca1
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-08-07 10:21:42 +0800

    modify numpy version installation

--- scripts/modelbench/version_collect.sh
+++ scripts/modelbench/version_collect.sh
@@ -20,8 +20,14 @@ else
     echo torch,${torch_branch},$(git rev-parse --short HEAD) >>${curdir}/${LOG_DIR}/version.csv
 fi
 
+# numpy 2.0 ValueError: numpy.dtype size changed, may indicate binary incompatibility. Expected 96 from C header, got 88 from PyObject
+pip install numpy==1.26.4
+
 echo torchvision,main,$(python -c "import torchvision; print(torchvision.__version__)") >>${curdir}/${LOG_DIR}/version.csv
 echo torchtext,main,$(python -c "import torchtext; print(torchtext.__version__)") >>${curdir}/${LOG_DIR}/version.csv
 echo torchaudio,main,$(python -c "import torchaudio; print(torchaudio.__version__)") >>${curdir}/${LOG_DIR}/version.csv
 echo torchdata,main,$(python -c "import torchdata; print(torchdata.__version__)") >>${curdir}/${LOG_DIR}/version.csv
 echo dynamo_benchmarks,main,$DYNAMO_BENCH >>${curdir}/${LOG_DIR}/version.csv
+
+# Recover numpy
+pip install --upgrade numpy
