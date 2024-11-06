000351279dfa417a6e6afce1da5f83ca99645fd5
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-11-07 02:27:06 +0800

    Update cpu_usebm.sh

--- scripts/userbenchmark/cpu_usebm.sh
+++ scripts/userbenchmark/cpu_usebm.sh
@@ -1,6 +1,6 @@
 export LD_PRELOAD=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libiomp5.so:${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libjemalloc.so
 export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:-1,muzzy_decay_ms:-1"
-export KMP_AFFINITY=granularity=fine,compact,1,0
+# export KMP_AFFINITY=granularity=fine,compact,1,0
 export KMP_BLOCKTIME=1
 export OMP_NUM_THREADS=40
 LOG_DIR=${1:-inductor_log}
@@ -25,27 +25,27 @@ if [[ ${mem_allowed_list} =~ '-' ]];then
     mem_allowed_list=$(echo ${mem_allowed_list} | awk -F- '{print $1}')
 fi
 # FP32 eager
-taskset -c 0-39 ${cmd_prefix}
+numactl -C 0-39 -m 0 ${cmd_prefix}
 mv .userbenchmark/cpu eager_throughtput_fp32_infer
 mv eager_throughtput_fp32_infer userbenchmark_aws/
 
 # BF16 eager
-taskset -c 0-39 ${cmd_prefix} --precision amp_bf16
+numactl -C 0-39 -m 0 ${cmd_prefix} --precision amp_bf16
 mv .userbenchmark/cpu eager_throughtput_bf16_infer
 mv eager_throughtput_bf16_infer userbenchmark_aws/
 
 # fx_int8 eager
-taskset -c 0-39 ${cmd_prefix} --precision fx_int8
+numactl -C 0-39 -m 0 ${cmd_prefix} --precision fx_int8
 mv .userbenchmark/cpu eager_throughtput_fx_int8
 mv eager_throughtput_fx_int8 userbenchmark_aws/
 
 # FP32 jit with llga:
-taskset -c 0-39 ${cmd_prefix} --backend torchscript --fuser fuser3
+numactl -C 0-39 -m 0 ${cmd_prefix} --backend torchscript --fuser fuser3
 mv .userbenchmark/cpu jit_llga_throughtput_fp32
 mv jit_llga_throughtput_fp32 userbenchmark_aws/
 
 # bf16 jit with llga:
-taskset -c 0-39 ${cmd_prefix} --precision amp_bf16 --backend torchscript --fuser fuser3
+numactl -C 0-39 -m 0 ${cmd_prefix} --precision amp_bf16 --backend torchscript --fuser fuser3
 mv .userbenchmark/cpu jit_llga_throughtput_amp_bf16
 mv jit_llga_throughtput_amp_bf16 userbenchmark_aws/
 
@@ -55,12 +55,12 @@ cmd_prefix='''python run_benchmark.py cpu -m BERT_pytorch,Background_Matting,Lea
 #cmd_prefix='''python run_benchmark.py cpu --test train --channels-last --launcher --launcher-args="--throughput-mode" --metrics throughputs'''
 
 # FP32 eager
-taskset -c 0-39 ${cmd_prefix}
+numactl -C 0-39 -m 0 ${cmd_prefix}
 mv .userbenchmark/cpu eager_throughtput_fp32_train
 mv eager_throughtput_fp32_train userbenchmark_aws/
 
 # BF16 eager
-taskset -c 0-39 ${cmd_prefix} --precision amp_bf16
+numactl -C 0-39 -m 0 ${cmd_prefix} --precision amp_bf16
 mv .userbenchmark/cpu eager_throughtput_bf16_train
 mv eager_throughtput_bf16_train userbenchmark_aws/
 
