56483db3caccd178f8544631bffab0dce8a2a131
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-11-10 19:24:20 +0800

    Update cpu_usebm_infer.sh

--- scripts/userbenchmark/cpu_usebm_infer.sh
+++ scripts/userbenchmark/cpu_usebm_infer.sh
@@ -1,6 +1,6 @@
 export LD_PRELOAD=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libiomp5.so:${CONDA_PREFIX:-"$(dirname $(which conda))/../"}/lib/libjemalloc.so
 export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:-1,muzzy_decay_ms:-1"
-export KMP_AFFINITY=granularity=fine,compact,1,0
+# export KMP_AFFINITY=granularity=fine,compact,1,0
 export KMP_BLOCKTIME=1
 LOG_DIR=${1:-inductor_log}
 cd ../benchmark
