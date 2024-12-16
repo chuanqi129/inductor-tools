51f71855add7630d3329764d777acee866404638
LifengWang <lifeng.a.wang@intel.com>
2024-12-16 14:37:33 +0800

    refine the test script

--- scripts/windows_inductor/dynamo_test_cpp.ps1
+++ scripts/windows_inductor/dynamo_test_cpp.ps1
@@ -12,11 +12,11 @@ foreach ($d in $dirs) {
     }
 }
 
-$env:HTTP_PROXY="http://proxy.ims.intel.com:911"
-$env:HTTPS_PROXY="http://proxy.ims.intel.com:911"
-$env:TORCHINDUCTOR_FREEZING=1
-$env:TORCHINDUCTOR_CPP_WRAPPER=1
-$env:CXX=""
+$env:HTTP_PROXY = "http://proxy.ims.intel.com:911"
+$env:HTTPS_PROXY = "http://proxy.ims.intel.com:911"
+$env:TORCHINDUCTOR_FREEZING = 1
+$env:TORCHINDUCTOR_CPP_WRAPPER = 1
+$env:CXX = ""
 
 python benchmarks/dynamo/torchbench.py --performance --float32 -dcpu --output=$dir\cpp_msvc\torchbench_perf_test.csv --inference -n50 --inductor --timeout 9000 --freezing --dashboard -x llava -x simple_gpt_tp_manual -x stable_diffusion -x llama_v2_7b_16h -x hf_clip -x tacotron2 -x nanogpt -x hf_T5_generate -x torchrec_dlrm -x simple_gpt -x hf_Whisper -x stable_diffusion_text_encoder -x sam -x sam_fast -x detectron2_maskrcnn -x cm3leon_generate -x llama_v2_7b_16h --cold-start-latency 2>&1 | Tee-Object -FilePath "$dir\cpp_msvc\torchbench_perf_test_output.log"
 
@@ -30,7 +30,7 @@ python benchmarks/dynamo/timm_models.py --performance --float32 -dcpu --output=$
 
 python benchmarks/dynamo/timm_models.py --accuracy --float32 -dcpu --output=$dir\cpp_msvc\timm_models_accuracy_test.csv --inference -n50 --inductor --timeout 9000 --freezing --dashboard -x llama_v2_7b_16h --cold-start-latency 2>&1 | Tee-Object -FilePath "$dir\cpp_msvc\timm_models_accuracy_test_output.log"
 
-$env:CXX="icx-cl"
+$env:CXX = "icx-cl"
 python benchmarks/dynamo/torchbench.py --performance --float32 -dcpu --output=$dir\cpp_icc\torchbench_perf_test.csv --inference -n50 --inductor --timeout 9000 --freezing --dashboard -x llava -x simple_gpt_tp_manual -x stable_diffusion -x llama_v2_7b_16h -x hf_clip -x tacotron2 -x nanogpt -x hf_T5_generate -x torchrec_dlrm -x simple_gpt -x hf_Whisper -x stable_diffusion_text_encoder -x sam -x sam_fast -x detectron2_maskrcnn -x cm3leon_generate -x llama_v2_7b_16h --cold-start-latency 2>&1 | Tee-Object -FilePath "$dir\cpp_icc\torchbench_perf_test_output.log"
 
 python benchmarks/dynamo/torchbench.py --accuracy --float32 -dcpu --output=$dir\cpp_icc\torchbench_accuracy_test.csv --inference -n50 --inductor --timeout 9000 --freezing --dashboard -x llava -x simple_gpt_tp_manual -x stable_diffusion -x llama_v2_7b_16h -x hf_clip -x tacotron2 -x nanogpt -x hf_T5_generate -x torchrec_dlrm -x simple_gpt -x hf_Whisper -x stable_diffusion_text_encoder -x sam -x sam_fast -x detectron2_maskrcnn -x cm3leon_generate -x llama_v2_7b_16h --cold-start-latency 2>&1 | Tee-Object -FilePath "$dir\cpp_icc\torchbench_accuracy_test_output.log"
@@ -39,6 +39,6 @@ python benchmarks/dynamo/huggingface.py --performance --float32 -dcpu --output=$
 
 python benchmarks/dynamo/huggingface.py --accuracy --float32 -dcpu --output=$dir\cpp_icc\huggingface_accuracy_test.csv --inference -n50 --inductor --timeout 9000 --freezing --dashboard -x llama_v2_7b_16h --cold-start-latency 2>&1 | Tee-Object -FilePath "$dir\cpp_icc\huggingface_accuracy_test_output.log"
 
-python benchmarks/dynamo/timm_models.py --performance --float32 -dcpu --output=$dir\output_dir_default\cpp_icc\timm_models_perf_test.csv --inference -n50 --inductor --timeout 9000 --freezing --dashboard -x llava -x simple_gpt_tp_manual -x stable_diffusion -x llama_v2_7b_16h -x hf_clip -x tacotron2 -x nanogpt -x hf_T5_generate -x torchrec_dlrm -x simple_gpt -x hf_Whisper -x stable_diffusion_text_encoder -x sam -x sam_fast -x detectron2_maskrcnn -x cm3leon_generate -x llama_v2_7b_16h --cold-start-latency 2>&1 | Tee-Object -FilePath "$dir\cpp_icc\timm_models_perf_test_output.log"
+python benchmarks/dynamo/timm_models.py --performance --float32 -dcpu --output=$dir\cpp_icc\timm_models_perf_test.csv --inference -n50 --inductor --timeout 9000 --freezing --dashboard -x llava -x simple_gpt_tp_manual -x stable_diffusion -x llama_v2_7b_16h -x hf_clip -x tacotron2 -x nanogpt -x hf_T5_generate -x torchrec_dlrm -x simple_gpt -x hf_Whisper -x stable_diffusion_text_encoder -x sam -x sam_fast -x detectron2_maskrcnn -x cm3leon_generate -x llama_v2_7b_16h --cold-start-latency 2>&1 | Tee-Object -FilePath "$dir\cpp_icc\timm_models_perf_test_output.log"
 
-python benchmarks/dynamo/timm_models.py --accuracy --float32 -dcpu --output=$dir\output_dir_default\cpp_icc\timm_models_accuracy_test.csv --inference -n50 --inductor --timeout 9000 --freezing --dashboard -x llama_v2_7b_16h --cold-start-latency 2>&1 | Tee-Object -FilePath "$dir\cpp_icc\timm_models_accuracy_test_output.log"
+python benchmarks/dynamo/timm_models.py --accuracy --float32 -dcpu --output=$dir\cpp_icc\timm_models_accuracy_test.csv --inference -n50 --inductor --timeout 9000 --freezing --dashboard -x llama_v2_7b_16h --cold-start-latency 2>&1 | Tee-Object -FilePath "$dir\cpp_icc\timm_models_accuracy_test_output.log"
