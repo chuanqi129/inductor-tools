038dbd81484a9df48425de796463e5a8e7d52771
LifengWang <lifeng.a.wang@intel.com>
2025-01-07 14:59:19 +0800

    refactor conda environment creation logic and update script parameters

--- groovy/inductor_windows.groovy
+++ groovy/inductor_windows.groovy
@@ -2,7 +2,7 @@
 properties([
     parameters([
         string(name: 'NODE_LABEL', defaultValue: 'clx137', description: '', trim: true),
-        string(name: 'create_conda_env', defaultValue: true, description: '', trim: true),
+        string(name: 'create_conda_env', defaultValue: "true", description: '', trim: true),
         string(name: 'conda_env_name', defaultValue: 'pt_win', description: '', trim: true),
         string(name: 'recipients', defaultValue: 'lifeng.a.wang@intel.com', description: '', trim: true),
     ])
@@ -12,14 +12,20 @@ node(NODE_LABEL) {
     stage("prepare the environment") {
         deleteDir()
         checkout scm
-        pwsh '''
-        Set-Location "$env:WORKSPACE"
-        cmd.exe "/K" (
+        if(params.create_conda_env)
+        {
+            pwsh '''
+            cmd.exe "/K" (
             '"C:/Program Files (x86)/Intel/oneAPI/setvars.bat" ' +
             '&& pwsh -File inductor-tools/scripts/windows_inductor/prepare_env_nightly.ps1 ' +
-            '-create_conda_env ${create_conda_env} ' +
             '-envName ${conda_env_name}'
-        )
+            )
+            '''
+        }
+
+        pwsh '''
+        Set-Location "$env:WORKSPACE"
+        git clone --depth=1 https://github.com/pytorch/pytorch.git
         '''
     }
 }
--- scripts/windows_inductor/prepare_env_nightly.ps1
+++ scripts/windows_inductor/prepare_env_nightly.ps1
@@ -1,40 +1,37 @@
 param (
     [string]$envName = "pt_win",
-    [string]$pythonVersion = "3.10",
-    [bool]$create_conda_env = $true
+    [string]$pythonVersion = "3.10"
 )
 
 $env:HTTP_PROXY = "http://proxy.ims.intel.com:911"
 $env:HTTPS_PROXY = "http://proxy.ims.intel.com:911"
 $env:DISTUTILS_USE_SDK = 1
 
-git clone --depth=1 https://github.com/pytorch/pytorch.git
 
+# Create a new conda environment
+Write-Host "Creating conda environment: $envName with Python $pythonVersion"
+
+# Check if the conda environment exists and remove it if it does
 $envList = conda env list | Select-String -Pattern "^\s*$envName\s"
 
-if ($create_conda_env or !$envList) {
-    # Create a new conda environment
-    Write-Host "Creating conda environment: $envName with Python $pythonVersion"
-
-    # Check if the conda environment exists and remove it if it does
-    if ($envList) {
-        Write-Host "Environment $envName exists. Removing it..."
-        conda env remove -y -n $envName
-    }
-    conda create -y -n $envName python=$pythonVersion
-
-    # Activate the new environment
-    Write-Host "Activating conda environment: $envName"
-    conda activate $envName
-
-    pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
-
-    git clone --depth=1 https://github.com/pytorch/benchmark.git
-    Set-Location benchmark
-    pip install --no-deps -r requirements.txt
-    pip install  safetensors portalocker tokenizers==0.19 huggingface_hub regex botocore ninja
-
-    python install.py --continue_on_fail --no-build-isolation
+if ($envList) {
+    Write-Host "Environment $envName exists. Removing it..."
+    conda env remove -y -n $envName
 }
+conda create -y -n $envName python=$pythonVersion
+
+# Activate the new environment
+Write-Host "Activating conda environment: $envName"
+conda activate $envName
+
+pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
+
+git clone --depth=1 https://github.com/pytorch/benchmark.git
+Set-Location benchmark
+pip install --no-deps -r requirements.txt
+pip install  safetensors portalocker tokenizers==0.19 huggingface_hub regex botocore ninja
+
+python install.py --continue_on_fail --no-build-isolation
+
 
 
