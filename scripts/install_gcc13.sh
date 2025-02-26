b74a681e17dba89b9c44ef14d6a15b34a7304ca9
LifengWang <lifeng.a.wang@intel.com>
2025-02-26 10:09:10 +0800

    Update GCC installation script to version 13.3 and hardcode libstdc++.so.6 library path

--- scripts/install_gcc13.sh
+++ scripts/install_gcc13.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 set -x
-conda install -y -c conda-forge gcc_linux-64=13.1.0 gxx_linux-64=13.1.0
-ln -sf /opt/conda/libexec/gcc/x86_64-conda-linux-gnu/13.1.0/gcc /usr/bin/gcc
-which gcc
-gcc --version
+conda install -y -c conda-forge gcc_linux-64=13.3 gxx_linux-64=13.3  gcc=13.3 gxx=13.3
+# hardcode for the libstdc++.so.6 library.
+ln -sf /opt/conda/lib/libstdc++.so.6.0.33 /lib/x86_64-linux-gnu/libstdc++.so.6
