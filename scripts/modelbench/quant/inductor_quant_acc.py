4d1bd4a30e9d7d792affc8d804d65764da98cb3d
mengfeil <mengfei.li@intel.com>
2025-02-27 10:15:56 +0800

    format

--- scripts/modelbench/quant/inductor_quant_acc.py
+++ scripts/modelbench/quant/inductor_quant_acc.py
@@ -4,16 +4,22 @@ import torch
 import torchvision.models as models
 import torch._inductor as torchinductor
 import copy
-from torch.ao.quantization.quantize_pt2e import prepare_pt2e, convert_pt2e, prepare_qat_pt2e
+from torch.ao.quantization.quantize_pt2e import (
+    prepare_pt2e,
+    convert_pt2e,
+    prepare_qat_pt2e,
+)
 import torch.ao.quantization.quantizer.x86_inductor_quantizer as xiq
 import torch.ao.quantization.quantizer.xpu_inductor_quantizer as xpuiq
 from torch.export import export_for_training
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+
 class AverageMeter(object):
     """Computes and stores the average and current value"""
-    def __init__(self, name, fmt=':f'):
+
+    def __init__(self, name, fmt=":f"):
         self.name = name
         self.fmt = fmt
         self.reset()
@@ -31,7 +37,7 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
     def __str__(self):
-        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
         return fmtstr.format(**self.__dict__)
 
 
@@ -58,27 +64,36 @@ def run_model(model_name, args):
         print("using cpp_wrapper")
         torchinductor.config.cpp_wrapper = args.cpp_wrapper
     valdir = args.dataset_dir
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
     val_loader = torch.utils.data.DataLoader(
-    datasets.ImageFolder(valdir, transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        normalize,
-    ])),
-    batch_size=50, shuffle=False,
-    num_workers=4, pin_memory=True)
+        datasets.ImageFolder(
+            valdir,
+            transforms.Compose(
+                [
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    normalize,
+                ]
+            ),
+        ),
+        batch_size=50,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True,
+    )
     cal_loader = copy.deepcopy(val_loader)
     model = models.__dict__[model_name](pretrained=True)
     if args.is_qat:
         model = model.train()
     else:
         model = model.eval()
-    top1 = AverageMeter('Acc@1', ':6.2f')
-    top5 = AverageMeter('Acc@5', ':6.2f')
-    quant_top1 = AverageMeter('Acc@1', ':6.2f')
-    quant_top5 = AverageMeter('Acc@5', ':6.2f')
+    top1 = AverageMeter("Acc@1", ":6.2f")
+    top5 = AverageMeter("Acc@5", ":6.2f")
+    quant_top1 = AverageMeter("Acc@1", ":6.2f")
+    quant_top5 = AverageMeter("Acc@5", ":6.2f")
     x = torch.randn(50, 3, 224, 224).contiguous(memory_format=torch.channels_last)
     example_inputs = (x,)
 
@@ -87,26 +102,35 @@ def run_model(model_name, args):
         print("using qat")
         for i, (images, _) in enumerate(cal_loader):
             exported_model = export_for_training(model, (images,)).module()
-            if i==10: break
+            if i == 10:
+                break
         quantizer = xiq.X86InductorQuantizer()
-        quantizer.set_global(xiq.get_default_x86_inductor_quantization_config(is_qat=True))
+        quantizer.set_global(
+            xiq.get_default_x86_inductor_quantization_config(is_qat=True)
+        )
         prepared_model = prepare_qat_pt2e(exported_model, quantizer)
         lr = 0.0001
         momentum = 0.9
         weight_decay = 1e-4
-        optimizer = torch.optim.SGD(prepared_model.parameters(), lr, momentum=momentum, weight_decay=weight_decay)
+        optimizer = torch.optim.SGD(
+            prepared_model.parameters(),
+            lr,
+            momentum=momentum,
+            weight_decay=weight_decay,
+        )
         optimizer.zero_grad()
         criterion = torch.nn.CrossEntropyLoss()
         for i, (images, target) in enumerate(val_loader):
-        # print(" start QAT Calibration step: {}".format(i), flush=True)
+            # print(" start QAT Calibration step: {}".format(i), flush=True)
             images = images
             target = target
             output = prepared_model(images)
             loss = criterion(output, target)
             optimizer.zero_grad()
             loss.backward()
-            optimizer.step()               
-            if i == 1: break
+            optimizer.step()
+            if i == 1:
+                break
 
         with torch.no_grad():
             converted_model = convert_pt2e(prepared_model)
@@ -117,7 +141,7 @@ def run_model(model_name, args):
         print("using fp32")
         model = model.to(args.device)
         with torch.no_grad():
-            optimized_model = torch.compile(model)     
+            optimized_model = torch.compile(model)
     else:
         print("using ptq")
         model = model.to(args.device)
@@ -126,9 +150,11 @@ def run_model(model_name, args):
         with torch.no_grad():
             exported_model = export_for_training(model, example_inputs).module()
             quantizer = None
-            if args.device == 'xpu':
+            if args.device == "xpu":
                 quantizer = xpuiq.XPUInductorQuantizer()
-                quantizer.set_global(xpuiq.get_default_xpu_inductor_quantization_config())
+                quantizer.set_global(
+                    xpuiq.get_default_xpu_inductor_quantization_config()
+                )
             else:
                 quantizer = xiq.X86InductorQuantizer()
                 quantizer.set_global(xiq.get_default_x86_inductor_quantization_config())
@@ -150,46 +176,68 @@ def run_model(model_name, args):
             quant_top1.update(quant_acc1[0], images.size(0))
             quant_top5.update(quant_acc5[0], images.size(0))
         if args.is_fp32:
-            print(model_name + " fp32: " + ' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-                .format(top1=quant_top1, top5=quant_top5))
+            print(
+                model_name
+                + " fp32: "
+                + " * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}".format(
+                    top1=quant_top1, top5=quant_top5
+                )
+            )
         else:
-            print(model_name + " int8: " + ' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-                .format(top1=quant_top1, top5=quant_top5))
+            print(
+                model_name
+                + " int8: "
+                + " * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}".format(
+                    top1=quant_top1, top5=quant_top5
+                )
+            )
 
 
 if __name__ == "__main__":
-    model_list=["alexnet","mnasnet1_0","mobilenet_v2","mobilenet_v3_large","resnet152",
-                "resnet18","resnet50","resnext50_32x4d","shufflenet_v2_x1_0","squeezenet1_1","vgg16"]
+    model_list = [
+        "alexnet",
+        "mnasnet1_0",
+        "mobilenet_v2",
+        "mobilenet_v3_large",
+        "resnet152",
+        "resnet18",
+        "resnet50",
+        "resnext50_32x4d",
+        "shufflenet_v2_x1_0",
+        "squeezenet1_1",
+        "vgg16",
+    ]
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--device",
-        default='cpu',
+        default="cpu",
         help="Device to run",
     )
     parser.add_argument(
         "--quantize",
-        action='store_true',
+        action="store_true",
         help="enable quantize for inductor",
     )
     parser.add_argument(
         "--cpp_wrapper",
-        action='store_true',
+        action="store_true",
         help="enable cpp wrapper for inductor",
     )
     parser.add_argument(
         "--is_qat",
-        action='store_true',
+        action="store_true",
         help="enable qat quantization for inductor",
     )
     parser.add_argument(
         "--is_fp32",
-        action='store_true',
+        action="store_true",
         help="fp32 inductor",
     )
     parser.add_argument(
         "--dataset_dir",
-        default='/workspace/benchmark/imagenet/val/',
+        default="/workspace/benchmark/imagenet/val/",
         help="ImageNet dir",
     )
     parser.add_argument(
