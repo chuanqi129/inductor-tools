7420660626366c21d11f14a4fc6ffa27941e6e99
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-03-02 17:32:45 +0800

    Update torchtune_xpu.sh

--- scripts/modelbench/torchtune_xpu.sh
+++ scripts/modelbench/torchtune_xpu.sh
@@ -52,7 +52,7 @@ tune download meta-llama/Meta-Llama-3-8B-Instruct --output-dir /tmp/Meta-Llama-3
 tune run lora_finetune_single_device --config llama3/8B_lora_single_device device=xpu dtype=$dtype max_steps_per_epoch=$iter seed=123 2>&1 | tee torchtune_log/Meta-Llama-3-8B-Instruct_lora.log
 #meta-llama/Meta-Llama-3-8B-Instruct full
 tune download meta-llama/Meta-Llama-3-8B-Instruct --output-dir /tmp/Meta-Llama-3-8B-Instruct
-tune run lora_finetune_single_device --config llama3/8B_full_single_device device=xpu dtype=$dtype max_steps_per_epoch=$iter optimizer._component_=torchao.prototype.low_bit_optim.AdamWFp8 seed=123 2>&1 | tee torchtune_log/Meta-Llama-3-8B-Instruct_full.log
+tune run full_finetune_single_device --config llama3/8B_full_single_device device=xpu dtype=$dtype max_steps_per_epoch=$iter optimizer._component_=torchao.prototype.low_bit_optim.AdamWFp8 seed=123 2>&1 | tee torchtune_log/Meta-Llama-3-8B-Instruct_full.log
 if [[ $dtype == 'bf16' ]]; then
     #meta-llama/Meta-Llama-3.1-8B-Instruct qlora finetune 300 step
     tune run lora_finetune_single_device --config llama3_1/8B_qlora_single_device device=xpu dtype=$dtype max_steps_per_epoch=300 2>&1 | tee torchtune_log/Meta-Llama-3-8B-Instruct_qlora_300.log
