e7c099d6290a9c2dcdb8190607e3e30b10a8f94a
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-04-22 10:52:51 +0800

    Update env.sh

--- scripts/modelbench/distributed/env.sh
+++ scripts/modelbench/distributed/env.sh
@@ -5,4 +5,3 @@
 . /opt/intel/oneapi/pti/latest/env/vars.sh
 . /home/sdp/intel_oneccl/oneapi/ccl/latest/env/vars.sh
 . /home/sdp/intel_oneccl/oneapi/mpi/latest/env/vars.sh
-. /opt/intel/oneapi/mkl/latest/env/vars.sh
