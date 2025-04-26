fca6926ddcf79106cccd5bb46c21513abe3621a3
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-04-26 23:45:09 +0800

    Update test.sh

--- scripts/modelbench/distributed/test.sh
+++ scripts/modelbench/distributed/test.sh
@@ -1,28 +1,30 @@
 #FSDP
+LOG_DIR = "/home/sdp/xiangdong/2025ww17_e2e"
 git clone -b release/xpu/2.6.10 https://github.com/intel/intel-extension-for-pytorch.git ipex
 cd ipex/examples/gpu/llm/fine-tuning
 pip install -r requirements.txt
 cd Llama3
-git clone https://github.com/huggingface/accelerate.git
+#git clone https://github.com/huggingface/accelerate.git
 cd accelerate
-
 pip install -e .
 export CCL_PROCESS_LAUNCHER=none
 export TORCH_LLM_ALLREDUCE=1
 export model="meta-llama/Meta-Llama-3-8B"
 #full fine-tuning
-accelerate launch --config_file "fsdp_config.yaml" llama3_ft.py --model_name_or_path ${model} --use_flashattn False --bf16 True --max_seq_length 256 --output_dir="output" --learning_rate=1e-3 --gradient_accumulation_steps=1 --per_device_train_batch_size=2 --per_device_eval_batch_size=2 --num_train_epochs=1 --save_steps=500 --logging_steps=1 --save_total_limit=8
+accelerate launch --config_file "fsdp_config.yaml" llama3_ft.py --model_name_or_path ${model} --use_flashattn False --bf16 True --max_seq_length 256 --output_dir="output" --learning_rate=1e-3 --gradient_accumulation_steps=1 --per_device_train_batch_size=2 --per_device_eval_batch_size=2 --num_train_epochs=1 --save_steps=500 --logging_steps=1 --save_total_limit=8 2>&1 | tee ${LOG_DIR}/Meta-Llama-3-8B_full_finetune.log
 #LoRA fine-tuning
-accelerate launch --config_file "fsdp_config.yaml" llama3_ft.py --model_name_or_path ${model} --use_flashattn False --bf16 True --use_peft True --max_seq_length 128 --output_dir="output" --learning_rate=1e-3 --gradient_accumulation_steps=1 --per_device_train_batch_size=8 --per_device_eval_batch_size=8 --num_train_epochs=1 --save_steps=500 --logging_steps=1 --save_total_limit=8 
+accelerate launch --config_file "fsdp_config.yaml" llama3_ft.py --model_name_or_path ${model} --use_flashattn False --bf16 True --use_peft True --max_seq_length 128 --output_dir="output" --learning_rate=1e-3 --gradient_accumulation_steps=1 --per_device_train_batch_size=8 --per_device_eval_batch_size=8 --num_train_epochs=1 --save_steps=500 --logging_steps=1 --save_total_limit=8 2>&1 | tee ${LOG_DIR}/Meta-Llama-3-8B_lora_finetune.log
 #FSDP2
-git clone -b benchmark https://github.com/zxd1997066/torchtune.git
-git clone https://github.com/pytorch/ao.git
-cd ao
-pip install -e .
-cd ../torchtune
-pip install -e .
+# git clone -b benchmark https://github.com/zxd1997066/torchtune.git
+# git clone https://github.com/pytorch/ao.git
+# cd ao
+# pip install -e .
+# cd ../torchtune
+# pip install -e .
 #meta-llama/Meta-Llama-3.1-8B-Instruct lora
-tune run --nproc_per_node 4 lora_finetune_distributed --config llama3_1/8B_lora device=xpu dtype=bf16 max_steps_per_epoch=10 seed=123 dataset.packed=True tokenizer.max_seq_len=512
+tune run --nproc_per_node 4 lora_finetune_distributed --config llama3_1/8B_lora device=xpu dtype=bf16 max_steps_per_epoch=10 seed=123 dataset.packed=True tokenizer.max_seq_len=512 2>&1 | tee ${LOG_DIR}/Meta-Llama-3.1-8B-Instruct_lora_4c.log
+tune run --nproc_per_node 2 lora_finetune_distributed --config llama3_1/8B_lora device=xpu dtype=bf16 max_steps_per_epoch=10 seed=123 dataset.packed=True tokenizer.max_seq_len=512 2>&1 | tee ${LOG_DIR}/Meta-Llama-3.1-8B-Instruct_lora_2c.log
+#FSDP2
 #meta-llama/Meta-Llama-3.1-8B-Instruct qlora
 tune run --nnodes 1 --nproc_per_node 2 lora_finetune_distributed --config llama3_1/8B_qlora_single_device device=xpu dtype=bf16 max_steps_per_epoch=10 seed=123 dataset.packed=True tokenizer.max_seq_len=512
 #meta-llama/Meta-Llama-3-8B-Instruct dora
