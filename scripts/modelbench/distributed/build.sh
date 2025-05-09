bd287b646c13878964ec350a049cf98e10221c83
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-05-09 16:18:55 +0800

    Update build.sh

--- scripts/modelbench/distributed/build.sh
+++ scripts/modelbench/distributed/build.sh
@@ -12,6 +12,7 @@ source /opt/intel/oneapi/umf/latest/env/vars.sh
 source /opt/intel/oneapi/pti/latest/env/vars.sh
 source /opt/intel/oneapi/ccl/latest/env/vars.sh
 source /opt/intel/oneapi/mpi/latest/env/vars.sh
+source /opt/intel/oneapi/mkl/latest/env/vars.sh
 WERROR=1 python setup.py bdist_wheel 2>&1 | tee /var/lib/jenkins/workspace/distributed_log/pytorch_build.log >/dev/null
 python -m pip install patchelf
 rm -rf ./tmp
