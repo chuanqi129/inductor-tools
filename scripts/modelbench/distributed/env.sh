ecd02d7d50878828178f665a7acf7d41bb45a671
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-05-24 22:58:10 +0800

    Update env.sh

--- scripts/modelbench/distributed/env.sh
+++ scripts/modelbench/distributed/env.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-source /opt/intel/oneapi/compiler/latest/env/vars.sh
-source /opt/intel/oneapi/umf/latest/env/vars.sh
-source /opt/intel/oneapi/pti/latest/env/vars.sh
-source /opt/intel/oneapi/ccl/latest/env/vars.sh
-source /opt/intel/oneapi/mpi/latest/env/vars.sh
-source /opt/intel/oneapi/mkl/latest/env/vars.sh
+# source /opt/intel/oneapi/compiler/latest/env/vars.sh
+# source /opt/intel/oneapi/umf/latest/env/vars.sh
+# source /opt/intel/oneapi/pti/latest/env/vars.sh
+# source /opt/intel/oneapi/ccl/latest/env/vars.sh
+# source /opt/intel/oneapi/mpi/latest/env/vars.sh
+# source /opt/intel/oneapi/mkl/latest/env/vars.sh
+source /home/sdp/intel/oneapi/compiler/latest/env/vars.sh
+source /home/sdp/intel/oneapi/umf/latest/env/vars.sh
+source /home/sdp/intel/oneapi/pti/latest/env/vars.sh
+source /home/sdp/intel/oneapi/ccl/latest/env/vars.sh
+source /home/sdp/intel/oneapi/mpi/latest/env/vars.sh
+source /home/sdp/intel/oneapi/mkl/latest/env/vars.sh
