a103cd75faa9b8f0c5ee80533e3de73719426b81
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-05-25 17:16:23 +0800

    Update env.sh

--- scripts/modelbench/distributed/env.sh
+++ scripts/modelbench/distributed/env.sh
@@ -9,7 +9,7 @@ source /opt/intel/oneapi/mkl/latest/env/vars.sh
 # source /home/sdp/intel/oneapi/compiler/latest/env/vars.sh
 # source /home/sdp/intel/oneapi/umf/latest/env/vars.sh
 # source /home/sdp/intel/oneapi/pti/latest/env/vars.sh
-# source /home/sdp/intel/oneapi/ccl/latest/env/vars.sh
-# source /home/sdp/intel/oneapi/mpi/latest/env/vars.sh
+source /home/sdp/intel/oneapi/ccl/latest/env/vars.sh
+source /home/sdp/intel/oneapi/mpi/latest/env/vars.sh
 # source /home/sdp/intel/oneapi/mkl/latest/env/vars.sh
-source /home/sdp/xiangdong/libraries.performance.communication.oneccl-release-ccl_2021.16-gold/build/_install/env/vars.sh
+# source /home/sdp/xiangdong/libraries.performance.communication.oneccl-release-ccl_2021.16-gold/build/_install/env/vars.sh
