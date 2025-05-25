f21fb7f5bc4bc4c81fc25fa7088769fc0868b6f5
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-05-25 17:46:51 +0800

    Update build.sh

--- scripts/modelbench/distributed/build.sh
+++ scripts/modelbench/distributed/build.sh
@@ -12,7 +12,7 @@ source /opt/intel/oneapi/compiler/latest/env/vars.sh
 source /opt/intel/oneapi/umf/latest/env/vars.sh
 source /opt/intel/oneapi/pti/latest/env/vars.sh
 # source /opt/intel/oneapi/ccl/latest/env/vars.sh
-# source /opt/intel/oneapi/mpi/latest/env/vars.sh
+source /opt/intel/oneapi/mpi/latest/env/vars.sh
 # source /home/sdp/intel/oneapi/compiler/latest/env/vars.sh
 # source /home/sdp/intel/oneapi/umf/latest/env/vars.sh
 # source /home/sdp/intel/oneapi/pti/latest/env/vars.sh
