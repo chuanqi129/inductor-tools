ba295e094499da943c0d6664666fe36782529838
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-06-13 16:36:14 +0800

    Update build.sh

--- scripts/modelbench/distributed/build.sh
+++ scripts/modelbench/distributed/build.sh
@@ -8,17 +8,17 @@ export USE_KINETO=OFF
 cd /var/lib/jenkins/workspace/pytorch/
 pip install -r requirements.txt
 pip install mkl-static mkl-include
-# source /opt/intel/oneapi/compiler/latest/env/vars.sh
-# source /opt/intel/oneapi/umf/latest/env/vars.sh
-# source /opt/intel/oneapi/pti/latest/env/vars.sh
-# source /opt/intel/oneapi/ccl/latest/env/vars.sh
-# source /opt/intel/oneapi/mpi/latest/env/vars.sh
-source /home/sdp/intel/oneapi/compiler/latest/env/vars.sh
-source /home/sdp/intel/oneapi/umf/latest/env/vars.sh
-source /home/sdp/intel/oneapi/pti/latest/env/vars.sh
+source /opt/intel/oneapi/compiler/latest/env/vars.sh
+source /opt/intel/oneapi/umf/latest/env/vars.sh
+source /opt/intel/oneapi/pti/latest/env/vars.sh
+source /opt/intel/oneapi/ccl/latest/env/vars.sh
+source /opt/intel/oneapi/mpi/latest/env/vars.sh
+# source /home/sdp/intel/oneapi/compiler/latest/env/vars.sh
+# source /home/sdp/intel/oneapi/umf/latest/env/vars.sh
+# source /home/sdp/intel/oneapi/pti/latest/env/vars.sh
 # source /home/sdp/intel/oneapi/ccl/latest/env/vars.sh
-source /home/sdp/intel/oneapi/mpi/latest/env/vars.sh
-source /home/sdp/xiangdong/libraries.performance.communication.oneccl-release-ccl_2021.16-gold/build/_install/env/vars.sh
+# source /home/sdp/intel/oneapi/mpi/latest/env/vars.sh
+# source /home/sdp/xiangdong/libraries.performance.communication.oneccl-release-ccl_2021.16-gold/build/_install/env/vars.sh
 source /opt/rh/gcc-toolset-11/enable
 python setup.py bdist_wheel 2>&1 | tee /var/lib/jenkins/workspace/distributed_log/pytorch_build.log >/dev/null
 python -m pip install patchelf
